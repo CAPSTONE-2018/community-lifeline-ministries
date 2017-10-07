950ef64b32a5fd4436c58bfce2ae7c1c4e368176
Gregory Patel <gregorypatel@front.cs.lewisu.edu>
2017-10-07 14:44:19 -0500

    modified:   AddingEmergency.php 	modified:   AddingParent.php 	modified:   AddingSchedule.php 	modified:   AddingStudent.php 	modified:   AddingTeacher.php 	modified:   Register.php 	modified:   StudentAdded.php 	modified:   add.css 	modified:   adding.css 	new file:   bootstrap-3.3.7-dist/css/bootstrap-theme.css 	new file:   bootstrap-3.3.7-dist/css/bootstrap-theme.css.map 	new file:   bootstrap-3.3.7-dist/css/bootstrap-theme.min.css 	new file:   bootstrap-3.3.7-dist/css/bootstrap-theme.min.css.map 	new file:   bootstrap-3.3.7-dist/css/bootstrap.css 	new file:   bootstrap-3.3.7-dist/css/bootstrap.css.map 	new file:   bootstrap-3.3.7-dist/css/bootstrap.min.css 	new file:   bootstrap-3.3.7-dist/css/bootstrap.min.css.map 	new file:   bootstrap-3.3.7-dist/fonts/glyphicons-halflings-regular.eot 	new file:   bootstrap-3.3.7-dist/fonts/glyphicons-halflings-regular.svg 	new file:   bootstrap-3.3.7-dist/fonts/glyphicons-halflings-regular.ttf 	new file:   bootstrap-3.3.7-dist/fonts/glyphicons-halflings-regular.woff 	new file:   bootstrap-3.3.7-dist/fonts/glyphicons-halflings-regular.woff2 	new file:   bootstrap-3.3.7-dist/js/bootstrap.js 	new file:   bootstrap-3.3.7-dist/js/bootstrap.min.js 	new file:   bootstrap-3.3.7-dist/js/npm.js
    Implemented bootstrap in order improve each form's layout.

--- AddingEmergency.php
+++ AddingEmergency.php
@@ -8,6 +8,7 @@
 <html>
     <head>
         <link rel="stylesheet" type="text/css" href="adding.css"/>
+        <link rel="stylesheet" type="text/css" href="bootstrap-3.3.7-dist/css/bootstrap.min.css">
     </head>
     
     <body>
@@ -19,19 +20,36 @@
         
 		<img src="Lifeline.png" alt="Community Lifeline" align="middle">
 
+		<div id="form_wrapper">
         <form action="EmergencyAdded.php" method="POST" id="form2">
 			<br>
+			
 			<label><h2>Add Emergency Contact Information:</h2></label><br>
-			<label><h4>Student ID Number:</h4><input type="text" name="id"></label><br><br>
-			<label><h4>Student First Name:</h4><input type="text" name="studf"></label><br><br>
-			<label><h4>Student Last Name:</h4><input type="text" name="studl"></label><br><br>
-			<label><h4>Emergency Contact First Name:</h4><input type="text" name="fname"></label><br><br>
-            <label><h4>Emergency Contact Last Name:</h4><input type="text" name="lname"></label><br><br>
-			<label><h4>Relationship:</h4><input type="text" name="rel"></label><br><br>
-            <label><h4>Phone Number:</h4><input type="text" name="pnum"></label><br><br>
-			<input type="submit" value="Submit"><br><br> 
+			<label><h4>Student Name:</h4>
+                <input placeholder="First" type="text" name="studf">
+                <input placeholder="Last" type="text" name="studl">
+            </label>
+            <br><br>
+            
+            <label><h4>Student ID Number:</h4><input type="text" name="id"></label>
+            <br><br>
+            
+			<label>
+                <h4>Emergency Contact Name:</h4>
+                <input placeholder="First" type="text" name="fname">
+                <input placeholder="Last" type="text" name="lname">
+            </label>
+            <br><br>
+            
+			<label><h4>Relationship:</h4><input type="text" name="rel"></label>
+			<br><br>
+			
+            <label><h4>Phone Number:</h4><input type="text" name="pnum"></label>
+            <br><br>
+            
+			<input id="submit" type="submit" value="Submit"><br><br> 
         </form>
-        
+        </div>
     </body>
 
 </html>
--- AddingSchedule.php
+++ AddingSchedule.php
@@ -8,6 +8,7 @@
 <html>
     <head>
         <link rel="stylesheet" type="text/css" href="adding.css"/>
+        <link rel="stylesheet" type="text/css" href="bootstrap-3.3.7-dist/css/bootstrap.min.css">
     </head>
     
     <body>
@@ -19,21 +20,38 @@
         
 		<img src="Lifeline.png" alt="Community Lifeline" align="middle">
         
+        <div id="form_wrapper">
         <form action="ScheduleAdded.php" method="POST" id="form2">
 			<br>
 			<label><h2>Add Schedule Information:</h2></label><br>
-			<label><h4>Class Name:</h4><input type="text" name="class_name"></label><br><br>
-			<label><h4>Teacher ID Number:</h4><input type="text" name="teacher"></label><br><br>
-			<label><h4>Teacher First Name:</h4><input type="text" name="teacher_f"></label><br><br>
-			<label><h4>Teacher Last Name:</h4><input type="text" name="teacher_l"></label><br><br>
-            <label><h4>Student ID Number:</h4><input type="text" name="id_student"></label><br><br>
-            <label><h4>Student First Name:</h4><input type="text" name="studfirst"></label><br><br>
-            <label><h4>Student Last Name:</h4><input type="text" name="studlast"></label><br><br>
-            <label><h4>Room Number:</h4><input type="text" name="room_num"></label><br><br>
-            <label><h4>Class Time (HH:MMAM/PM):</h4><input type="text" name="class_time"></label><br><br>
+			<label><h4>Class Name:</h4><input type="text" name="class_name"></label>
+			<br><br>
+			
+			<label><h4>Room Number:</h4><input type="text" name="room_num"></label>
+			<label><h4>Class Time:</h4><input placeholder="HH:MMAM/PM" type="text" name="class_time"></label>
+			<br><br>
+			
+			<label><h4>Teacher Name:</h4>
+                <input placeholder="First" type="text" name="teacher_f">
+                <input placeholder="Last" type="text" name="teacher_l">
+            </label>
+            <br><br>
+            
+            <label><h4>Teacher ID Number:</h4><input type="text" name="teacher"></label>
+            <br><br>
+            
+            <label><h4>Student Name:</h4>
+                <input placeholder="First" type="text" name="studfirst">
+                <input placeholder="Last" type="text" name="studlast">
+            </label>
+            <br><br>
+            
+            <label><h4>Student ID Number:</h4><input type="text" name="id_student"></label>
+            <br><br>
 
-            <input type="submit" value="Submit"><br><br>
+            <input id="submit" type="submit" value="Submit"><br><br>
         </form> 
+        </div>
     
     </body>
 
