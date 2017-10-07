984fad6be8f28c9d67a44c15695aafd0e4591315
Gregory Patel <gregorypatel@front.cs.lewisu.edu>
2017-10-07 14:59:20 -0500

    Added same formatting to search forms that is used for the addition forms.

--- SearchingInfo.php
+++ SearchingInfo.php
@@ -8,6 +8,7 @@
 <html>
     <head>
         <link rel="stylesheet" type="text/css" href="adding.css"/>
+        <link rel="stylesheet" type="text/css" href="bootstrap-3.3.7-dist/css/bootstrap.min.css">
     </head>
     
     <body>
@@ -19,15 +20,16 @@
         
 		<img src="Lifeline.png" alt="Community Lifeline" align="middle">
         
+        <div id="form_wrapper">
         <form action="InfoSearched.php" method="POST" id="form2">
 			<br>
 			<label><h2>Search Student/Parent/Emergency Contact Information:</h2></label><br>
 			<label><h4>Student Last Name:</h4><input type="text" name="name"></label><br><br>
 			<label><h4>Student ID Number:</h4><input type="text" name="child_id"></label><br><br>
 			
-            <input type="submit" value="Submit"><br><br>
+            <input id="submit" type="submit" value="Submit"><br><br>
 		</form>
-
+        </div>
     </body>
 
 </html>
--- SearchingSchedule.php
+++ SearchingSchedule.php
@@ -8,6 +8,7 @@
 <html>
     <head>
         <link rel="stylesheet" type="text/css" href="adding.css"/>
+        <link rel="stylesheet" type="text/css" href="bootstrap-3.3.7-dist/css/bootstrap.min.css">
     </head>
     
     <body>
@@ -19,13 +20,14 @@
         
 		<img src="Lifeline.png" alt="Community Lifeline" align="middle">
         
+        <div id="form_wrapper">
         <form action="ScheduleSearched.php" method="POST" id="form2">
 			<br>
 			<label><h2>Search Schedule Information:</h2></label><br>
             <label><h4>Student ID Number:</h4><input type="text" name="id_student"></label><br><br>
-			<input type="submit" value="Submit"><br><br>
+			<input id="submit" type="submit" value="Submit"><br><br>
 		</form>
-
+        </div>
     </body>
 
 </html>
