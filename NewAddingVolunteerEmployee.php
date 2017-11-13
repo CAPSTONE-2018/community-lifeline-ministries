fdc2bcd86f7089a186230aa6207764a383a25679
Matthew Vitt <matthewrvitt@front.cs.lewisu.edu>
2017-11-13 13:23:42 -0600

    issue 21 complete

--- NewAddingVolunteerEmployee.php
+++ NewAddingVolunteerEmployee.php
@@ -12,112 +12,148 @@ include("Header.php");
     <link rel="stylesheet" type="text/css" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
 </head>
 
-<body>
+    <body>
 
-<div id="form_wrapper">
-    <form action="NewVolunteerEmployeeAdded.php" method="POST" id="form2">
-        <br>
-        <label><h2>Add Volunteer/Employee Information:</h2></label><br>
-        <label><h4>Volunteer/Employee Name:</h4>
-            <input placeholder="First" type="text" name="fname">
-            <input placeholder="Last" type="text" name="lname">
-        </label>
-        <br><br>
+        <div class="container">
 
-        <label><h4>Volunteer/Employee Id:</h4><input type="text" name="id"></label>
+            <div id="form_wrapper">
+                <form class="form-horizontal" action="NewVolunteerEmployeeAdded.php" method="POST" id="form2">
 
-        <label><h4>Cell Phone:</h4><input type="text" name="cellphone"></label>
-        <label><h4>Home Phone:</h4><input type="text" name="homephone"></label>
-        <br><br>
+                    <h1>Add Volunteer/Employee Information:</h1>
+                    <br />
+                    <div class="row">
 
-        <label><h4>Address:</h4><input type="text" name="address"></label>
-        <label><h4>City:</h4><input type="text" name="city"></label>
-        <label><h4>State:</h4>
+                        <div class="form-group">
+                            <div class="col-lg-4">
+                                <label class="control-label" for="first">First Name:</label>
+                                <input id="first" class="form-control" placeholder="First Name" type="text" name="fname">
+                            </div>
+                            <div class="col-lg-4">
+                                <label class="control-label" for="last">Last Name</label>
+                                <input id="last" class="form-control" placeholder="Last Name" type="text" name="lname">
+                            </div>
+                            <div class="col-lg-4">
+                                <label class="control-label" for="veid">Volunteer/Employee Id:</label>
+                                <input id="veid" class="form-control" placeholder="Volunteer/Employee ID" type="text" name="id">
+                            </div>
+                        </div>
+                    </div>
+                    <div class="row">
 
-            <select name="state">
-                <option value="AL">Alabama</option>
-                <option value="AK">Alaska</option>
-                <option value="AZ">Arizona</option>
-                <option value="AR">Arkansas</option>
-                <option value="CA">California</option>
-                <option value="CO">Colorado</option>
-                <option value="CT">Connecticut</option>
-                <option value="DE">Delaware</option>
-                <option value="FL">Florida</option>
-                <option value="GA">Georgia</option>
-                <option value="HI">Hawaii</option>
-                <option value="ID">Idaho</option>
-                <option value="IL">Illinois</option>
-                <option value="IN">Indiana</option>
-                <option value="IA">Iowa</option>
-                <option value="KS">Kansas</option>
-                <option value="KY">Kentucky</option>
-                <option value="LA">Louisiana</option>
-                <option value="ME">Maine</option>
-                <option value="MD">Maryland</option>
-                <option value="MA">Massachusetts</option>
-                <option value="MI">Michigan</option>
-                <option value="MN">Minnesota</option>
-                <option value="MS">Mississippi</option>
-                <option value="MO">Missouri</option>
-                <option value="MT">Montana</option>
-                <option value="NE">Nebraska</option>
-                <option value="NV">Nevada</option>
-                <option value="NH">New Hampshire</option>
-                <option value="NJ">New Jersey</option>
-                <option value="NM">New Mexico</option>
-                <option value="NY">New York</option>
-                <option value="NC">North Carolina</option>
-                <option value="ND">North Dakota</option>
-                <option value="OH">Ohio</option>
-                <option value="OK">Oklahoma</option>
-                <option value="OR">Oregon</option>
-                <option value="PA">Pennsylvania</option>
-                <option value="RI">Rhode Island</option>
-                <option value="SC">South Carolina</option>
-                <option value="SD">South Dakota</option>
-                <option value="TN">Tennessee</option>
-                <option value="TX">Texas</option>
-                <option value="UT">Utah</option>
-                <option value="VT">Vermont</option>
-                <option value="VA">Virginia</option>
-                <option value="WA">Washington</option>
-                <option value="WV">West Virginia</option>
-                <option value="WI">Wisconsin</option>
-                <option value="WY">WYoming</option>
-            </select>
+                        <div class="form-group">
+                            <div class="col-lg-6">
+                                <label class="control-label" for="cell">Cell Phone:</label>
+                                <input id="cell" class="form-control" placeholder="Cell Phone" type="text" name="cellphone">
+                            </div>
+                            <div class="col-lg-6">
+                                <label class="control-label" for="homephone">Home Phone:</label>
+                                <input id="homephone" class="form-control" placeholder="Home Phone" type="text" name="homephone">
+                            </div>
+                        </div>
+                    </div>
+                    <div class="row">
 
-        </label>
-        <label><h4>Zip:</h4><input type="text" name="zip"></label>
-        <br><br>
+                        <div class="form-group">
+                            <div class="col-lg-3">
+                                <label class="control-label" for="address">Address:</label>
+                                <input id="address" class="form-control" placeholder="Address" type="text" name="address">
+                            </div>
+                            <div class="col-lg-3">
+                                <label class="control-label" for="city">City:</label>
+                                <input id="city" class="form-control" placeholder="City" type="text" name="city">
+                            </div>
+                            <div class="col-lg-3">
+                                <label class="control-label" for="state">State:</label>
+                                <select id="state" class="form-control" name="state">
+                                    <option value="AL">Alabama</option>
+                                    <option value="AK">Alaska</option>
+                                    <option value="AZ">Arizona</option>
+                                    <option value="AR">Arkansas</option>
+                                    <option value="CA">California</option>
+                                    <option value="CO">Colorado</option>
+                                    <option value="CT">Connecticut</option>
+                                    <option value="DE">Delaware</option>
+                                    <option value="FL">Florida</option>
+                                    <option value="GA">Georgia</option>
+                                    <option value="HI">Hawaii</option>
+                                    <option value="ID">Idaho</option>
+                                    <option value="IL">Illinois</option>
+                                    <option value="IN">Indiana</option>
+                                    <option value="IA">Iowa</option>
+                                    <option value="KS">Kansas</option>
+                                    <option value="KY">Kentucky</option>
+                                    <option value="LA">Louisiana</option>
+                                    <option value="ME">Maine</option>
+                                    <option value="MD">Maryland</option>
+                                    <option value="MA">Massachusetts</option>
+                                    <option value="MI">Michigan</option>
+                                    <option value="MN">Minnesota</option>
+                                    <option value="MS">Mississippi</option>
+                                    <option value="MO">Missouri</option>
+                                    <option value="MT">Montana</option>
+                                    <option value="NE">Nebraska</option>
+                                    <option value="NV">Nevada</option>
+                                    <option value="NH">New Hampshire</option>
+                                    <option value="NJ">New Jersey</option>
+                                    <option value="NM">New Mexico</option>
+                                    <option value="NY">New York</option>
+                                    <option value="NC">North Carolina</option>
+                                    <option value="ND">North Dakota</option>
+                                    <option value="OH">Ohio</option>
+                                    <option value="OK">Oklahoma</option>
+                                    <option value="OR">Oregon</option>
+                                    <option value="PA">Pennsylvania</option>
+                                    <option value="RI">Rhode Island</option>
+                                    <option value="SC">South Carolina</option>
+                                    <option value="SD">South Dakota</option>
+                                    <option value="TN">Tennessee</option>
+                                    <option value="TX">Texas</option>
+                                    <option value="UT">Utah</option>
+                                    <option value="VT">Vermont</option>
+                                    <option value="VA">Virginia</option>
+                                    <option value="WA">Washington</option>
+                                    <option value="WV">West Virginia</option>
+                                    <option value="WI">Wisconsin</option>
+                                    <option value="WY">WYoming</option>
+                                </select>
+                            </div>
+
+                            <div class="col-lg-3">
+                                <label class="control-label" for="zip">Zip:</label>
+                                <input id="zip" class="form-control" placeholder="Zip Code" type="text" name="zip">
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="row">
+
+                        <div class="form-group">
+                            <div class="col-lg-4">
+                                <label class="control-label" for="email">Email:</label>
+                                <input id="email" class="form-control" placeholder="Email" type="text" name="email">
+                            </div>
+                            <div class="col-lg-4">
+                                <label class="control-label" for="type">Type:</label>
+                                    <select id="type" class="form-control" name="type">
+                                        <option value="Volunteer">Volunteer</option>
+                                        <option value="Employee">Employee</option>
+                                    </select>
+                            </div>
+                            <div class="col-lg-4">
+                                <label class="control-label" for="program">Program:</label>
+                                    <select id="program" class="form-control" name="program">
+                                        <option value="G.E.M.S">G.E.M.S</option>
+                                        <option value="Sons of Thunder">Sons of Thunder</option>
+                                    </select>
+                            </div>
 
 
-        <label><h4>Email:</h4><input type="text" name="email"></label>
-        <br><br>
+                        </div>
+                    </div>
 
-        <label><h4>Type:</h4>
-
-            <select name="type">
-                <option value="Volunteer">Volunteer</option>
-                <option value="Employee">Employee</option>
-            </select>
-
-        </label>
-        <br><br>
-
-        <label><h4>Program:</h4>
-
-            <select name="program">
-                <option value="G.E.M.S">G.E.M.S</option>
-                <option value="Sons of Thunder">Sons of Thunder</option>
-            </select>
-
-        </label>
-        <br><br>
-
-        <input id="submit" type="submit" value="Submit"><br><br>
-    </form>
-</div>
-
-</body>
+                    <input id="submit" class="btn btn-primary btn-lg btn-block" type="submit" value="Submit"><br><br>
+                </form>
+            </div>
+        </div>
+    </body>
 </html>
--- NewVolunteerEmployeeAdded.php
+++ NewVolunteerEmployeeAdded.php
@@ -9,57 +9,64 @@ include("Header.php");
 <html>
 <head>
     <link rel="stylesheet" type="text/css" href="add.css"/>
+    <link rel="stylesheet" type="text/css" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
 </head>
 
-<body>
-
-<label><h3>Add Test Score Information:</h3></label><br>
-
-
-<?php
-
-//Setting up variable to use for mysqli function
-$server = "localhost";
-$user = "clm_user";
-$psw = "dbuser";
-$database = "community_lifeline";
-//Connecting to database
-$db = mysqli_connect($server, $user, $psw, $database);
-if (!$db) {
-    print "Error - Could not connect to MySQL";
-    exit;
-}
+    <body>
+        <div class="container">
+            <h1>Add Test Score Information:</h1>
+            <br />
 
 
+            <?php
 
-$id = intval($_POST['id']);
-$fname = $_POST['fname'];
-$lname = $_POST['lname'];
-$cellphone = $_POST['cellphone'];
-$homephone = $_POST['homephone'];
-$address = $_POST['address'];
-$city = $_POST['city'];
-$state = $_POST['state'];
-$zip = intval($_POST['zip']);
-$email = $_POST['email'];
-$type = $_POST['type'];
-$program = $_POST['program'];
+            //Setting up variable to use for mysqli function
+            $server = "localhost";
+            $user = "clm_user";
+            $psw = "dbuser";
+            $database = "community_lifeline";
+            //Connecting to database
+            $db = mysqli_connect($server, $user, $psw, $database);
+            if (!$db) {
+                print "Error - Could not connect to MySQL";
+                exit;
+            }
 
 
 
+            $id = intval($_POST['id']);
+            $fname = $_POST['fname'];
+            $lname = $_POST['lname'];
+            $cellphone = $_POST['cellphone'];
+            $homephone = $_POST['homephone'];
+            $address = $_POST['address'];
+            $city = $_POST['city'];
+            $state = $_POST['state'];
+            $zip = intval($_POST['zip']);
+            $email = $_POST['email'];
+            $type = $_POST['type'];
+            $program = $_POST['program'];
 
-$stmt = $db->prepare("INSERT INTO Volunteer_Employee VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)");
-$stmt->bind_param('isssssssisss', $id, $fname, $lname, $cellphone, $homephone, $address, $city, $state, $zip, $email, $type, $program);
-$stmt->execute();
 
 
-if ($stmt->affected_rows == -1) {
-    echo "<h2>Volunteer/Employee could not be added to the database, please try again.</h2>";
-}
-else {
-    echo "<h2>Volunteer/Employee has been successfully added to the database.</h2>";
-    $stmt->close();
-}
-?>
-</body>
+
+            $stmt = $db->prepare("INSERT INTO Volunteer_Employee VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)");
+            $stmt->bind_param('isssssssisss', $id, $fname, $lname, $cellphone, $homephone, $address, $city, $state, $zip, $email, $type, $program);
+            $stmt->execute();
+
+
+            if ($stmt->affected_rows == -1) {
+                echo "<div class='alert alert-danger'>
+                        <strong>Failure! </strong>Volunteer/Employee could not be added to the database, please try again.
+                      </div>";
+            }
+            else {
+                echo "<div class='alert alert-success'>
+                        <strong>Success! </strong>Volunteer/Employee has been successfully added to the database.
+                      </div>";
+                $stmt->close();
+            }
+            ?>
+        </div>
+    </body>
 </html>
