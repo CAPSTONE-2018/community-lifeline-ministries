47cfeef53c40d86177f4d257588e5e65e1fe94f8
Matthew Vitt <matthewrvitt@front.cs.lewisu.edu>
2017-11-13 14:34:11 -0600

    issue24 complete

--- NewAddingTestScores.php
+++ NewAddingTestScores.php
@@ -12,33 +12,56 @@ include("Header.php");
     <link rel="stylesheet" type="text/css" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
 </head>
 
-<body>
+    <body>
+        <div class="container">
+            <div id="form_wrapper">
+                <form class="form-horizontal" action="NewTestScoresAdded.php" method="POST" id="form2">
 
-<div id="form_wrapper">
-    <form action="NewTestScoresAdded.php" method="POST" id="form2">
-        <br>
+                    <h1>Add Student Test Scores:</h1>
+                    <br />
+
+                    <div class="row">
+
+                        <div class="form-group">
+                            <div class="col-lg-4">
+                                <label class="control-label" for="sid">Student ID Number:</label>
+                                <input id="sid" class="form-control" placeholder="Student ID" type="text" name="id">
+                            </div>
+                            <div class="col-lg-4">
+                                <label class="control-label" for="term">Term:</label>
+                                <select id="term" class="form-control" name="term">
+                                    <option value="Spring">Spring</option>
+                                    <option value="Summer">Summer</option>
+                                    <option value="Fall">Fall</option>
+                                    <option value="Winter">Winter</option>
+                                </select>
+                            </div>
+                            <div class="col-lg-4">
+                                <label class="control-label" for="year">Year:</label>
+                                <input id="year" class="form-control" placeholder="Year" type="text" name="year">
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="row">
+
+                        <div class="form-group">
+                            <div class="col-lg-6">
+                                <label class="control-label" for="pre">Pre-Test Score:</label>
+                                <input id="pre" class="form-control" placeholder="Pre-Test Score" type="text" name="pre_test">
+                            </div>
+                            <div class="col-lg-6">
+                                <label class="control-label" for="post">Post-Test Score:</label>
+                                <input id="post" class="form-control" placeholder="Post_Test Score" type="text" name="post_test">
+                            </div>
+                        </div>
+                    </div>
 
 
-        <label><h4>Student ID Number:</h4><input type="text" name="id"></label>
-        <br><br>
-
-        <label><h4>Term:</h4>
-            <select name="term">
-                <option value="Spring">Spring</option>
-                <option value="Summer">Summer</option>
-                <option value="Fall">Fall</option>
-                <option value="Winter">Winter</option>
-            </select>
-        </label>
-        <label><h4>Year:</h4><input type="text" name="year"></label>
-
-        <label><h4>Pre-Test Score:</h4><input type="text" name="pre_test"></label>
-        <label><h4>Post-Test Score:</h4><input type="text" name="post_test"></label>
-        <br><br>
-
-        <input id="submit" type="submit" value="Submit"><br><br>
-    </form>
-</div>
-</body>
+                    <input id="submit" class="btn btn-primary btn-lg btn-block" type="submit" value="Submit"><br><br>
+                </form>
+            </div>
+        </div>
+    </body>
 
 </html>
--- NewTestScoresAdded.php
+++ NewTestScoresAdded.php
@@ -9,47 +9,54 @@ include("Header.php");
 <html>
 <head>
     <link rel="stylesheet" type="text/css" href="add.css"/>
+    <link rel="stylesheet" type="text/css" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
 </head>
 
-<body>
-
-<label><h3>Add Test Score Information:</h3></label><br>
-
-
-<?php
-
-//Setting up variable to use for mysqli function
-$server = "localhost";
-$user = "clm_user";
-$psw = "dbuser";
-$database = "community_lifeline";
-//Connecting to database
-$db = mysqli_connect($server, $user, $psw, $database);
-if (!$db) {
-    print "Error - Could not connect to MySQL";
-    exit;
-}
-
-$id = intval($_POST['id']);
-$term = $_POST['term'];
-$year = intval($_POST['year']);
-$preTest = floatval($_POST['pre_test']);
-$postTest = floatval($_POST['post_test']);
+    <body>
+        <div class="container">
+            <h1>Add Test Score Information:</h1>
+            <br />
 
 
+            <?php
 
-$stmt = $db->prepare("INSERT INTO School_Year (Student_Id, Term, Year, Pre_Test, Post_Test) VALUES (?, ?, ?, ?, ?)");
-$stmt->bind_param('isidd', $id, $term, $year, $preTest, $postTest);
-$stmt->execute();
+            //Setting up variable to use for mysqli function
+            $server = "localhost";
+            $user = "clm_user";
+            $psw = "dbuser";
+            $database = "community_lifeline";
+            //Connecting to database
+            $db = mysqli_connect($server, $user, $psw, $database);
+            if (!$db) {
+                print "Error - Could not connect to MySQL";
+                exit;
+            }
+
+            $id = intval($_POST['id']);
+            $term = $_POST['term'];
+            $year = intval($_POST['year']);
+            $preTest = floatval($_POST['pre_test']);
+            $postTest = floatval($_POST['post_test']);
 
 
-if ($stmt->affected_rows == -1) {
-    echo "<h2>Test Scores could not be added to the database, please try again.</h2>";
-}
-else {
-    echo "<h2>Test Scores have been successfully added to the database.</h2>";
-    $stmt->close();
-}
-?>
-</body>
+
+            $stmt = $db->prepare("INSERT INTO School_Year (Student_Id, Term, Year, Pre_Test, Post_Test) VALUES (?, ?, ?, ?, ?)");
+            $stmt->bind_param('isidd', $id, $term, $year, $preTest, $postTest);
+            $stmt->execute();
+
+
+            if ($stmt->affected_rows == -1) {
+                echo "<div class='alert alert-danger'>
+                        <strong>Failure! </strong>Test Scores could not be added to the database, please try again.
+                      </div>";
+            }
+            else {
+                echo "<div class='alert alert-success'>
+                        <strong>Success! </strong>Test Scores have been successfully added to the database.
+                      </div>";
+                $stmt->close();
+            }
+            ?>
+        </div>
+    </body>
 </html>
