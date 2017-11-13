9bd5948b3181efd05dee9ba03a7a30a92eca9d0e
Matthew Vitt <matthewrvitt@front.cs.lewisu.edu>
2017-11-13 13:50:11 -0600

    issue22 complete

--- NewAddingClass.php
+++ NewAddingClass.php
@@ -12,38 +12,70 @@ include("Header.php");
     <link rel="stylesheet" type="text/css" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
 </head>
 
-<body>
+    <body>
+        <div class="container">
 
-<div id="form_wrapper">
-    <form action="NewClassAdded.php" method="POST" id="form2">
-        <br>
+            <div id="form_wrapper">
 
-        <label><h2>Add Class Information:</h2></label><br>
-        <label><h4>Class Name:</h4>
-            <input placeholder="Class Name" type="text" name="name">
-        </label>
-        <br><br>
-        <input placeholder="Class Id" type="text" name="id">
-        <label><h4>Volunteer/Employee Id:</h4><input type="text" name="vid"></label>
+                <form class="form-horizontal" action="NewClassAdded.php" method="POST" id="form2">
 
-        <label><h4>Room Number:</h4><input type="text" name="rnum"></label>
-        <label><h4>Day:</h4>
-            <select name="day">
-                <option value="Monday">Monday</option>
-                <option value="Tuesday">Tuesday</option>
-                <option value="Wednesday">Wednesday</option>
-                <option value="Thursday">Thursday</option>
-                <option value="Friday">Friday</option>
-                <option value="Saturday">Saturday</option>
-                <option value="Sunday">Sunday</option>
-            </select>
-        </label>
-        <label><h4>Start Time:</h4><input type="text" name="stime"></label>
-        <label><h4>End Time:</h4><input type="text" name="etime"></label>
 
-        <input id="submit" type="submit" value="Submit"><br><br>
-    </form>
-</div>
-</body>
+                    <h1>Add Class Information:</h1>
+                    <br />
+
+                    <div class="row">
+                        <div class="form-group">
+                            <div class="col-lg-3">
+                                <label class="control-label" for="name">Class Name:</label>
+                                <input id="name" class="form-control" placeholder="Class Name" type="text" name="name">
+                            </div>
+                            <div class="col-lg-3">
+                                <label class="control-label" for="cid">Class ID:</label>
+                                <input id="cid" class="form-control" placeholder="Class ID" type="text" name="id">
+                            </div>
+                            <div class="col-lg-3">
+                                <label class="control-label" for="veid">Volunteer/Employee ID:</label>
+                                <input id="veid" class="form-control" placeholder="Volunteer/Employee ID" type="text" name="vid">
+                            </div>
+                            <div class="col-lg-3">
+                                <label class="control-label" for="roomnum">Room Number:</label>
+                                <input id="roomnum" class="form-control" placeholder="Room Number" type="text" name="rnum">
+                            </div>
+
+                        </div>
+                    </div>
+
+                    <div class="row">
+
+                        <div class="form-group">
+
+                            <div class="col-lg-4">
+                                <label class="control-label" for="day">Day:</label>
+                                <select id="day" class="form-control" name="day">
+                                    <option value="Monday">Monday</option>
+                                    <option value="Tuesday">Tuesday</option>
+                                    <option value="Wednesday">Wednesday</option>
+                                    <option value="Thursday">Thursday</option>
+                                    <option value="Friday">Friday</option>
+                                    <option value="Saturday">Saturday</option>
+                                    <option value="Sunday">Sunday</option>
+                                </select>
+                            </div>
+                            <div class="col-lg-4">
+                                <label class="control-label" for="stime">Start Time:</label>
+                                <input id="stime" class="form-control" placeholder="Start Time" type="text" name="stime">
+                            </div>
+                            <div class="col-lg-4">
+                                <label class="control-label" for="etime">End Time:</label>
+                                <input id="etime" class="form-control" placeholder="End Time" type="text" name="etime">
+                            </div>
+                        </div>
+                    </div>
+
+                    <input id="submit" class="btn btn-primary btn-lg btn-block" type="submit" value="Submit"><br><br>
+                </form>
+            </div>
+        </div>
+    </body>
 
 </html>
--- NewClassAdded.php
+++ NewClassAdded.php
@@ -9,63 +9,74 @@ include("Header.php");
 <html>
 <head>
     <link rel="stylesheet" type="text/css" href="add.css"/>
+    <link rel="stylesheet" type="text/css" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
 </head>
 
-<body>
-
-<label><h3>Add Test Score Information:</h3></label><br>
-
-
-<?php
-
-//Setting up variable to use for mysqli function
-$server = "localhost";
-$user = "clm_user";
-$psw = "dbuser";
-$database = "community_lifeline";
-//Connecting to database
-$db = mysqli_connect($server, $user, $psw, $database);
-if (!$db) {
-    print "Error - Could not connect to MySQL";
-    exit;
-}
-
-$id = intval($_POST['id']);
-$className = $_POST['name'];
-$volunteerId = intval($_POST['vid']);
-$roomNum = $_POST['rnum'];
-$srtTime = $_POST['stime'];
-$endTime = $_POST['etime'];
-$day = $_POST['day'];
+    <body>
+        <div class="container">
+            <h1>Add Test Score Information:</h1>
+            <br />
 
 
+            <?php
 
-$stmt = $db->prepare("INSERT INTO Class VALUES (?, ?, ?)");
-$stmt->bind_param('isi', $id, $className, $volunteerId);
-$stmt->execute();
+            //Setting up variable to use for mysqli function
+            $server = "localhost";
+            $user = "clm_user";
+            $psw = "dbuser";
+            $database = "community_lifeline";
+            //Connecting to database
+            $db = mysqli_connect($server, $user, $psw, $database);
+            if (!$db) {
+                print "Error - Could not connect to MySQL";
+                exit;
+            }
+
+            $id = intval($_POST['id']);
+            $className = $_POST['name'];
+            $volunteerId = intval($_POST['vid']);
+            $roomNum = $_POST['rnum'];
+            $srtTime = $_POST['stime'];
+            $endTime = $_POST['etime'];
+            $day = $_POST['day'];
 
 
-if ($stmt->affected_rows == -1) {
-    echo "<h2>Class could not be added to the database, please try again.</h2>";
-}
-else {
-    echo "<h2>Class has been successfully added to the database.</h2>";
-    $stmt->close();
-}
 
-$stmt = $db->prepare("INSERT INTO Schedule (Class_Id, Room_Number, Start_Time, End_Time, Day) VALUES (?, ?, ?, ?, ?)");
-$stmt->bind_param('issss', $id, $roomNum, $srtTime, $endTime, $day);
-$stmt->execute();
+            $stmt = $db->prepare("INSERT INTO Class VALUES (?, ?, ?)");
+            $stmt->bind_param('isi', $id, $className, $volunteerId);
+            $stmt->execute();
 
 
-if ($stmt->affected_rows == -1) {
-    echo "<h2>Schedule could not be added to the database, please try again.</h2>";
-}
-else {
-    echo "<h2>Schedule has been successfully added to the database.</h2>";
-    $stmt->close();
-}
+            if ($stmt->affected_rows == -1) {
+                echo "<div class='alert alert-danger'>
+                        <strong>Failure! </strong>Class could not be added to the database, please try again.
+                      </div>";
+            }
+            else {
+                echo "<div class='alert alert-success'>
+                        <strong>Success! </strong>Class has been successfully added to the database.
+                      </div>";
+                $stmt->close();
+            }
 
-?>
-</body>
+            $stmt = $db->prepare("INSERT INTO Schedule (Class_Id, Room_Number, Start_Time, End_Time, Day) VALUES (?, ?, ?, ?, ?)");
+            $stmt->bind_param('issss', $id, $roomNum, $srtTime, $endTime, $day);
+            $stmt->execute();
+
+
+            if ($stmt->affected_rows == -1) {
+                echo "<div class='alert alert-danger'>
+                        <strong>Failure! </strong>Schedule could not be added to the database, please try again.
+                      </div>";
+            }
+            else {
+                echo "<div class='alert alert-success'>
+                        <strong>Success! </strong>Schedule has been successfully added to the database.
+                      </div>";
+                $stmt->close();
+            }
+
+            ?>
+        </div>
+    </body>
 </html>
