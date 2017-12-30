84d70913a2a656b9e4a3b7fb37063885fc0610c1
Cindy Howard <howardcy@lewisu.edu>
2017-12-30 14:45:20 -0600

    refactor

--- AdminPermissionsTest.php
+++ AdminPermissionsTest.php
@@ -1,29 +1,18 @@
- 
+
 <?php
 
-    //info to access database
-    $server = "localhost";
-    $user = "clm_user";
-    $psw = "dbuser";
-    $database = "community_lifeline";
-
-    //Connecting to database
-    $db = mysqli_connect($server, $user, $psw, $database);
-    if (!$db) {
-        print "Error - Could not connect to MySQL";
-        exit;
-    }
+  include("db/db.php");
     session_start();
     $username = $_SESSION['loggedIn'];
     if ($stmt = $db->prepare("SELECT * FROM Logins WHERE username=?")) {
-        
+
         $stmt->bind_param('s', $username);
-            
+
         $stmt->execute();
-            
+
         $stmt->bind_result($user_name, $account, $firstName, $lastName, $email, $pass);
         $stmt->fetch();
-        
+
         // Can only get to this page if you are logged in as an administrator
         if(!isset($_SESSION['loggedIn']) or $account !== "administrator"){
             $isAdmin = false;
--- login.php
+++ login.php
@@ -1,32 +1,21 @@
 <?php
-    //info to access database
-    $server = "localhost";
-    $user = "clm_user";
-    $psw = "dbuser";
-    $database = "community_lifeline";
-
-    //Connecting to database
-    $db = mysqli_connect($server, $user, $psw, $database);
-    if (!$db) {
-        print "Error - Could not connect to MySQL";
-        exit;
-    }
+    include("db/db.php");
 
     //saving info from text boxes
-    $username = $_POST['username'];    
+    $username = $_POST['username'];
     $password = $_POST['password'];
     $encryptPass = md5($password);
 
-    
+
     if ($stmt = $db->prepare("SELECT * FROM Logins WHERE username=? AND password=?")) {
-        
+
         $stmt->bind_param('ss', $username, $encryptPass);
-            
+
         $stmt->execute();
-            
+
         $stmt->bind_result($user_name, $account, $firstName, $lastName, $email, $pass);
         $stmt->fetch();
-        
+
         //checking if the credentials match an account in the database
         if($user_name == $username and $pass == $encryptPass and checkUsername($username) and checkPassword($password)) {
             session_start();
