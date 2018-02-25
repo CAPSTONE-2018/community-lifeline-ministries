3cacc262e5d0283e20f93eb97724dee2bd96890b
Nicholas MacRoy <nmacroy@gmail.com>
2018-02-25 09:23:16 -0600

    Update UpdateContact.php

--- UpdateContact.php
+++ UpdateContact.php
@@ -30,7 +30,7 @@ include("scripts/header.php");
     $relationship = $_POST['relationship'];
 
 
-    $sql = "UPDATE Contact SET First_Name = '$fname', Last_Name = '$lname', Middle_Name = '$manme', Suffix='$suffix',Phone_Cell = '$cellphone', Phone_Home = '$homephone', Address = '$address', City = '$city', State = '$state', Zip = '$zip', Email = '$email'  WHERE Id = '$id' ;";
+    $sql = "UPDATE Contact SET Prefix = '$prefix' ,First_Name = '$fname', Last_Name = '$lname', Middle_Name = '$manme', Suffix='$suffix',Phone_Cell = '$cellphone', Phone_Home = '$homephone', Address = '$address', City = '$city', State = '$state', Zip = '$zip', Email = '$email'  WHERE Id = '$id' ;";
 
     if ($db->query($sql) === TRUE){
         echo "<div class='alert alert-success'>
