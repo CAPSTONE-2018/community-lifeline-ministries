69c484b831056aa12e56c9ab46d8341640ff8875
joeyarbrough <jcyarbro88@gmail.com>
2018-02-25 12:55:29 -0600

    refactored to reflect database changes

--- php-files/new/NewClass.php
+++ php-files/new/NewClass.php
@@ -6,33 +6,30 @@ include("../scripts/header.php");
 include("../../db/config.php");
 
 
-$query = "SELECT * FROM Volunteer_Employee ORDER BY Last_Name, First_Name;";
+$query = "SELECT * FROM Volunteer_Employees ORDER BY Last_Name, First_Name;";
 $result = mysqli_query($db, $query);
 ?>
 
+<div class="container">
+
+    <div id="form_wrapper">
+
+        <form class="form-horizontal" action="../add/AddClass.php" method="POST" id="form2">
 
 
+            <h1>Add Class Information:</h1>
+            <br/>
 
-        <div class="container">
-
-            <div id="form_wrapper">
-
-                <form class="form-horizontal" action="../add/AddClass.php" method="POST" id="form2">
-
-
-                    <h1>Add Class Information:</h1>
-                    <br />
-
-                    <div class="row">
-                        <div class="form-group">
-                            <div class="col-lg-4">
-                                <label class="control-label" for="name">Class Name:</label>
-                                <input id="name" class="form-control" placeholder="Class Name" type="text" name="name">
-                            </div>
-                        </div>
+            <div class="row">
+                <div class="form-group">
+                    <div class="col-lg-4">
+                        <label class="control-label" for="name">Class Name:</label>
+                        <input id="name" class="form-control" placeholder="Class Name" type="text" name="name">
                     </div>
-
-                    <input id="submit" class="btn btn-primary btn-lg btn-block" type="submit" value="Submit"><br><br>
-                </form>
+                </div>
             </div>
-        <?php include("../../scripts/footer.php"); ?>
+
+            <input id="submit" class="btn btn-primary btn-lg btn-block" type="submit" value="Submit"><br><br>
+        </form>
+    </div>
+    <?php include("../scripts/footer.php"); ?>
