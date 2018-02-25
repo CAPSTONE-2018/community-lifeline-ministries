fba8a732ca44e699adf95c1b695cdf7e6c03b30f
joeyarbrough <jcyarbro88@gmail.com>
2018-02-25 12:42:28 -0600

    refactored to reflect database changes

--- php-files/add/AddClass.php
+++ php-files/add/AddClass.php
@@ -3,36 +3,31 @@
 include("../scripts/header.php");
 ?>
 
-            <h1>Add Test Score Information:</h1>
-            <br />
+<h1>Add Test Score Information:</h1>
+<br/>
 
 
-            <?php
+<?php
 
-            //connect to database
-            include("../../db/config.php");
+//connect to database
+include("../../db/config.php");
 
-            $id = intval($_POST['id']);
-            $className = $_POST['name'];
+$className = $_POST['name'];
 
-
-
-
-            $stmt = $db->prepare("INSERT INTO Classes (Id,Class_Name) VALUES (?, ?)");
-            $stmt->bind_param('is', $id ,$className);
-            $stmt->execute();
+$stmt = $db->prepare("INSERT INTO Classes (Class_Name) VALUES (?)");
+$stmt->bind_param('s', $className);
+$stmt->execute();
 
 
-            if ($stmt->affected_rows == -1) {
-                echo "<div class='alert alert-danger'>
+if ($stmt->affected_rows == -1) {
+    echo "<div class='alert alert-danger'>
                         <strong>Failure! </strong>Class could not be added to the database, please try again.
                       </div>";
-            }
-            else {
-                echo "<div class='alert alert-success'>
+} else {
+    echo "<div class='alert alert-success'>
                         <strong>Success! </strong>Class has been successfully added to the database.
                       </div>";
-                $stmt->close();
-            }
-            include("../scripts/footer.php");
-            ?>
+    $stmt->close();
+}
+include("../scripts/footer.php");
+?>
