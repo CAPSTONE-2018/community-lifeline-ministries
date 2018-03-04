ccccfdf5a5d20e03b51f23ff6d497037676c457f
joeyarbrough <jcyarbro88@gmail.com>
2018-03-03 18:56:37 -0600

    fixed the header and footer files so that every page will now have the bootstrap library through the <link /> at the top of the header.php file, and now the footer file properly closes

--- php-files/scripts/footer.php
+++ php-files/scripts/footer.php
@@ -1,6 +1,16 @@
-  </div>
-<!-- /container -->
 
+
+<!-- jQuery CDN -->
+<script src="https://code.jquery.com/jquery-1.12.0.min.js"></script>
+<!-- Bootstrap Js CDN -->
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+
+<script type="text/javascript">
+    $(document).ready(function () {
+        $('#sidebarCollapse').on('click', function () {
+            $('#sidebar').toggleClass('active');
+        });
+    });
+</script>
 </body>
-
 </html>
--- php-files/scripts/header.php
+++ php-files/scripts/header.php
@@ -1,5 +1,3 @@
-<link rel="stylesheet" type="text/css" href="../../css/clm.css"/>
-
 <?php
 session_start();
 if (!isset($_SESSION['loggedIn'])) {
