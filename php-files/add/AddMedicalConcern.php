9bac9b7a8989a2fbaf0e766e84fb1bdf7ba0504b
joeyarbrough <jcyarbro88@gmail.com>
2018-03-06 13:20:14 -0600

    changed AddAllergy and NewAllergy files to AddMedicalConcern and NewMedicalConcern.  Also renamed any reference of allergy, and reformatted the SQL insert statement

--- php-files/add/AddMedicalConcern.php
+++ php-files/add/AddMedicalConcern.php
@@ -10,23 +10,24 @@ include("../scripts/header.php");
 //connect to database
 include("../../db/config.php");
 
-$allergyName = $_POST['allergyName'];
-$allergyType = $_POST['allergyType'];
-$allergyNote = $_POST['allergyNote'];
+$userName = $_SESSION['loggedIn'];
+$medicalConcernName = $_POST['name'];
+$medicalConcernType = $_POST['type'];
+$medicalConcernNote = $_POST['note'];
 
-$stmt = $db->prepare("INSERT INTO Allergies (Name, Type, Note) VALUES (?, ?, ?)");
-$stmt->bind_param('sss', $allergyName, $allergyType, $allergyNote);
+$stmt = $db->prepare("INSERT INTO Medical_Concerns (Author_Username, Name, Type, Note) VALUES (?, ?, ?, ?)");
+$stmt->bind_param('ssss', $userName, $medicalConcernName, $medicalConcernType, $medicalConcernNote);
 $stmt->execute();
 
 if ($stmt->affected_rows == -1) {
     echo "
         <div class='alert alert-danger'>
-            <strong>Failure! </strong>Allergy could not be added to the database, please try again.
+            <strong>Failure! </strong>Medical Concern could not be added to the database, please try again.
         </div>";
 } else {
     echo "
         <div class='alert alert-success'>
-            <strong>Success! </strong>Allergy has been successfully added to the database.
+            <strong>Success! </strong>Medical Concern has been successfully added to the database.
         </div>";
     $stmt->close();
 }
--- php-files/new/NewMedicalConcern.php
+++ php-files/new/NewMedicalConcern.php
@@ -10,19 +10,19 @@ include("../../db/config.php");
 <div class="container">
     <div id="form_wrapper">
         <form class="form-horizontal" action="../add/AddMedicalConcern.php" method="POST" id="form2">
-            <h1>Allergies:</h1>
+            <h1>Medical Concerns:</h1>
             <br/>
             <div class="row">
                 <div class="form-group">
                     <div class="col-lg-6">
-                        <label class="control-label" for="allergyName">Allergy Name:</label>
-                        <input id="allergyName" class="form-control" placeholder="Allergy Name" type="text"
-                               name="allergyName">
+                        <label class="control-label" for="allergyName">Name:</label>
+                        <input id="allergyName" class="form-control" placeholder="Name" type="text"
+                               name="name">
                     </div>
                     <div class="col-lg-6">
                         <label class="control-label" for="allergyType">Type:</label>
-                        <input id="allergyType" class="form-control" placeholder="Allergy Type" type="text"
-                               name="allergyType">
+                        <input id="allergyType" class="form-control" placeholder="Type" type="text"
+                               name="type">
                     </div>
                 </div>
             </div>
@@ -31,7 +31,7 @@ include("../../db/config.php");
                     <div class="col-lg-11">
 
                         <label class="control-label" for="allergyNote">Note:</label>
-                        <textarea id="allergyNote" class="form-control" placeholder="Allergy Note" name="allergyNote"
+                        <textarea id="allergyNote" class="form-control" placeholder="Note" name="note"
                                   rows="4" cols="50">
                         </textarea>
                     </div>
