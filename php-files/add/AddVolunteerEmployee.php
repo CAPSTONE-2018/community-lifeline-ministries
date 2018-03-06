d9516900647b309e23cfe34ef36e74c7c89543cd
joeyarbrough <jcyarbro88@gmail.com>
2018-03-06 16:33:13 -0600

    adding a volunteer now takes in user making changes, and the active flag

--- php-files/add/AddVolunteerEmployee.php
+++ php-files/add/AddVolunteerEmployee.php
@@ -1,48 +1,50 @@
 <?php
-    include("../scripts/header.php");
+include("../scripts/header.php");
 ?>
 
 <h1>Add Volunteer/Employee:</h1>
-<br />
+<br/>
 
 <?php
 
-    //connect to database
-    include("../../db/config.php");
+//connect to database
+include("../../db/config.php");
+
+$userMakingChanges = $_SESSION['loggedIn'];
+$id = intval($_POST['id']);
+$prefix = $_POST['prefix'];
+$firstName = $_POST['firstName'];
+$lastName = $_POST['lastName'];
+$middleName = $_POST['middleName'];
+$suffix = $_POST['suffix'];
+$primaryPhone = $_POST['primaryPhone'];
+$secondaryPhone = $_POST['secondaryPhone'];
+$addressOne = $_POST['addressOne'];
+$addressTwo = $_POST['addressTwo'];
+$city = $_POST['city'];
+$state = $_POST['state'];
+$zip = intval($_POST['zip']);
+$email = $_POST['email'];
+$type = $_POST['type'];
+$isActiveFlag = $_POST['activeFlag'];
+
+$stmt = $db->prepare("INSERT INTO Volunteer_Employees (Author_Username, Active_Volunteer, Prefix, First_Name, Last_Name, Middle_Name, Suffix, Primary_Phone, Secondary_Phone, Address_One, Address_Two, City, State, Zip, Email, Type) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)");
+$stmt->bind_param('sisssssssssssiss', $userMakingChanges, $isActiveFlag, $prefix, $firstName, $lastName, $middleName, $suffix, $primaryPhone, $secondaryPhone, $addressOne, $addressTwo, $city, $state, $zip, $email, $type);
+$stmt->execute();
 
 
-    $id = intval($_POST['id']);
-    $prefix = $_POST['prefix'];
-    $firstName = $_POST['firstName'];
-    $lastName = $_POST['lastName'];
-    $middleName = $_POST['middleName'];
-    $suffix = $_POST['suffix'];
-    $cellPhone = $_POST['cellPhone'];
-    $homePhone = $_POST['homePhone'];
-    $address = $_POST['address'];
-    $city = $_POST['city'];
-    $state = $_POST['state'];
-    $zip = intval($_POST['zip']);
-    $email = $_POST['email'];
-    $type = $_POST['type'];
-
-    $stmt = $db->prepare("INSERT INTO Volunteer_Employees (Prefix, First_Name, Last_Name, Middle_Name, Suffix, Phone_Cell, Phone_Home, Address, City, State, Zip, Email, Type) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)");
-    $stmt->bind_param('ssssssssssiss', $prefix, $firstName, $lastName, $middleName, $suffix, $cellPhone, $homePhone, $address, $city, $state, $zip, $email, $type);
-    $stmt->execute();
-
-
-    if ($stmt->affected_rows == -1) {
-        echo "
+if ($stmt->affected_rows == -1) {
+    echo "
             <div class='alert alert-danger'>
                 <strong>Failure! </strong>Volunteer/Employee could not be added to the database, please try again.
             </div>";
-    } else {
-        echo "
+} else {
+    echo "
             <div class='alert alert-success'>
                 <strong>Success! </strong>Volunteer/Employee has been successfully added to the database.
             </div>";
-        $stmt->close();
-    }
+    $stmt->close();
+}
 
-    include("../scripts/footer.php");
+include("../scripts/footer.php");
 ?>
