7ed47f266aadd8ca862b765e743c74bfee07ded3
joeyarbrough <jcyarbro88@gmail.com>
2018-03-06 16:10:21 -0600

    add contact now submits user making change and active flag

--- php-files/new/NewContact.php
+++ php-files/new/NewContact.php
@@ -11,32 +11,47 @@
         <h1>Add Contact Information:</h1>
         <br/>
         <div class="row">
-            <div class="col-lg-4">
-                <label class="control-label" for="prefix">Prefix:</label>
-                <select id="gender" class="form-control" name="prefix">
-                    <option value="Mr.">Mr.</option>
-                    <option value="Ms.">Ms.</option>
-                    <option value="Mrs.">Mrs.</option>
-                </select>
-            </div>
-            <div class="col-lg-4">
-                <label class="control-label" for="suffix">Suffix:</label>
-                <input id="suffix" class="form-control" placeholder="Suffix" type="text" name="suffix">
-            </div>
-            <div class="col-lg-4">
-                <label class="control-label" for="studentId">Student ID Number For:</label>
 
-                <select id="studentId" class="form-control" name="id">
-                    <?php
-                    if (mysqli_num_rows($result) > 0) {
-                        while ($row = mysqli_fetch_assoc($result)) {
+            <div class="form-group">
+                <div class="col-lg-3">
+                    <label class="control-label" for="prefix">Prefix:</label>
+                    <select id="gender" class="form-control" name="prefix">
+                        <option value="Mr.">Mr.</option>
+                        <option value="Ms.">Ms.</option>
+                        <option value="Mrs.">Mrs.</option>
+                    </select>
+                </div>
+                <div class="col-lg-3">
+                    <label class="control-label" for="suffix">Suffix:</label>
+                    <input id="suffix" class="form-control" placeholder="Suffix" type="text" name="suffix">
+                </div>
+                <div class="col-lg-3">
+                    <label class="control-label" for="studentId">Student ID Number For:</label>
 
-                            echo "<option value='" . $row['Id'] . "'>" . $row['First_Name'] . " " . $row['Last_Name'] . "</option>";
+                    <select id="studentId" class="form-control" name="id">
+                        <?php
+                        if (mysqli_num_rows($result) > 0) {
+                            while ($row = mysqli_fetch_assoc($result)) {
+
+                                echo "<option value='" . $row['Id'] . "'>" . $row['First_Name'] . " " . $row['Last_Name'] . "</option>";
+                            }
                         }
-                    }
-                    ?>
-                </select>
+                        ?>
+                    </select>
+                </div>
+
+                <div class="col-lg-3">
+
+                        <label class="control-label" for="activeFlag">Active Contact?</label>
+                        <select id="activeFlag" class="form-control" name="activeFlag">
+                            <option value="1">Yes</option>
+                            <option value="0">No</option>
+                        </select>
+
+                </div>
+
             </div>
+
         </div>
 
         <div class="row">
@@ -61,13 +76,13 @@
         <div class="row">
             <div class="form-group">
                 <div class="col-lg-6">
-                    <label class="control-label" for="cellPhone">Cell Number:</label>
-                    <input id="cellPhone" class="form-control" placeholder="Cell Number" type="text" name="cellPhone" required />
+                    <label class="control-label" for="primaryPhone">Primary Phone Number:</label>
+                    <input id="primaryPhone" class="form-control" placeholder="Primary Number" type="text" name="primaryPhone" required />
                 </div>
 
                 <div class="col-lg-6">
-                    <label class="control-label" for="homePhone">Home Number:</label>
-                    <input id="homePhone" class="form-control" placeholder="Home Number" type="text" name="homePhone">
+                    <label class="control-label" for="secondaryPhone">Secondary Phone Number:</label>
+                    <input id="secondaryPhone" class="form-control" placeholder="Secondary Number" type="text" name="secondaryPhone">
                 </div>
 
             </div>
@@ -75,16 +90,23 @@
 
         <div class="row">
             <div class="form-group">
-                <div class="col-lg-3">
-                    <label class="control-label" for="address">Address:</label>
-                    <input id="address" class="form-control" placeholder="Address" type="text" name="address">
+                <div class="col-lg-6">
+                    <label class="control-label" for="addressOne">Address:</label>
+                    <input id="addressOne" class="form-control" placeholder="Address" type="text" name="addressOne">
                 </div>
-                <div class="col-lg-3">
+                <div class="col-lg-6">
+                    <label class="control-label" for="addressTwo">Apt/Suite:</label>
+                    <input id="addressTwo" class="form-control" placeholder="Address" type="text" name="addressTwo">
+                </div>
+            </div>
+
+            <div class="form-group">
+                <div class="col-lg-4">
                     <label class="control-label" for="city">City:</label>
                     <input id="city" class="form-control" placeholder="City" type="text" name="city">
                 </div>
 
-                <div class="col-lg-3">
+                <div class="col-lg-4">
                     <label class="control-label" for="state">State:</label>
                     <?php
                     include("../scripts/States.php");
@@ -92,7 +114,7 @@
                     ?>
                 </div>
 
-                <div class="col-lg-3">
+                <div class="col-lg-4">
                     <label class="control-label" for="zip">Zip Code:</label>
                     <input id="zip" class="form-control" placeholder="Zip Code" type="text" name="zip">
                 </div>
