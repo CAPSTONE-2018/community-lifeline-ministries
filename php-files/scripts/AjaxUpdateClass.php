4132e878883d4b55209eda1930c23014d8475b42
joeyarbrough <jcyarbro88@gmail.com>
2018-03-06 16:46:36 -0600

    updating a class now updates the Last_Updated_Timestamp column, along with loggin the user making the change

--- php-files/scripts/AjaxUpdateClass.php
+++ php-files/scripts/AjaxUpdateClass.php
@@ -20,6 +20,17 @@ $result2 = mysqli_query($db, $query2);
 $row2 = mysqli_fetch_array($result2);
 
 $classNameToDisplay = $row['Class_Name'];
+$isActiveFlag = $row['Active_Class'];
+
+
+$classIsActive = "";
+$classIsNotActive = "";
+
+if ($row['Active_Class'] == 1) {
+    $classIsActive = "selected=\"selected\"";
+} else {
+    $classIsNotActive = "selected=\"selected\"";
+}
 
 $output = "<div id=\"form_wrapper\">
     <form class=\"form - horizontal\" action=\"../update/UpdateClass.php\" method=\"POST\" id=\"form2\">
@@ -32,14 +43,19 @@ $output = "<div id=\"form_wrapper\">
                 <div class=\"col-lg-6\">
                     <label class=\"control-label\" for=\"name\">Class Name:</label>
                     <input id=\"name\" class=\"form-control\" value='".$row['Class_Name']."' placeholder=\"Class Name\" type=\"text\" name=\"name\">
-                </div>                 
+                </div>  
+                               
+                <div class=\"col-lg-6\">
+                    <label class=\"control-label\" for=\"activeFlag\">Active Class?</label>
+                    <select id=\"activeFlag\" class=\"form-control\" name=\"activeFlag\">
+                        <option value=\"1\">Yes</option>
+                        <option value=\"0\">No</option>
+                    </select>
+                </div>                               
             </div>
         </div>
-    
         <input id=\"submit\" class=\"btn btn-primary btn-lg btn-block\" type=\"submit\" value=\"Submit\"><br><br>
-
     </form>
-
 </div>";
 
 echo $output;
--- php-files/update/UpdateClass.php
+++ php-files/update/UpdateClass.php
@@ -1,42 +1,44 @@
 <?php
-    include("../scripts/header.php");
+include("../scripts/header.php");
 ?>
 <h1>Update Class Information:</h1>
-<br />
+<br/>
 
 <?php
 
-    //connect to database
-    include("../../db/config.php");
+//connect to database
+include("../../db/config.php");
 
-    session_start();
-    $id = $_SESSION['classId'];
-    $className = $_POST['name'];
+session_start();
+$userMakingChanges = $_SESSION['loggedIn'];
+$id = $_SESSION['classId'];
+$className = $_POST['name'];
+$isActiveFlag = $_POST['activeFlag'];
 
-    $sql = "UPDATE Classes SET Class_Name = '$className' WHERE Id = '$id' ;";
+$sql = "UPDATE Classes SET Author_Username = '$userMakingChanges', Last_Updated_Timestamp = NULL, Active_Class = $isActiveFlag, Class_Name = '$className' WHERE Id = '$id' ;";
 
-    if ($db->query($sql) === TRUE){
-        echo "
+if ($db->query($sql) === TRUE) {
+    echo "
             <div class='alert alert-success'>
                 <strong>Success! </strong>Class Name has been successfully Updated.
             </div>";
-    }else{
-        echo "
+} else {
+    echo "
             <div class='alert alert-danger'>
                 <strong>Failure! </strong>Class Name could not be updated, please try again.
             </div>";
-    }
+}
 
-    $sql2 = "UPDATE Schedule SET Room_Number = '$roomNum', Start_Time = '$srtTime', End_Time = '$endTime', Day = '$day' WHERE Class_Id = '$id';";
-    if ($db->query($sql2) === TRUE){
-        echo "<div class='alert alert-success'>
+$sql2 = "UPDATE Schedule SET Room_Number = '$roomNum', Start_Time = '$srtTime', End_Time = '$endTime', Day = '$day' WHERE Class_Id = '$id';";
+if ($db->query($sql2) === TRUE) {
+    echo "<div class='alert alert-success'>
                         <strong>Success! </strong>Class Schedule has been successfully Updated.
                       </div>";
-    }else{
-        echo "<div class='alert alert-danger'>
+} else {
+    echo "<div class='alert alert-danger'>
                         <strong>Failure! </strong>Class Schedule could not be updated, please try again.
                       </div>";
-    }
+}
 
-      include("../scripts/footer.php");
+include("../scripts/footer.php");
 ?>
