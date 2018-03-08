7a50f77fa21c9720b2a39ad078326dd7ecc13f0d
Josh <joshjurss@gmail.com>
2018-03-07 23:23:55 -0600

    Subtle Footer Change
    10---->11

--- php-files/scripts/footer.php
+++ php-files/scripts/footer.php
@@ -22,7 +22,7 @@
     $(document).ready(function () {
         $('#sidebarCollapse').on('click', function () {
             $('#sidebar').toggleClass('active');
-            $('#content').toggleClass('col-lg-10 shorten col-lg-9');
+            $('#content').toggleClass('col-lg-11 shorten col-lg-9');
         });
     });
 </script>
