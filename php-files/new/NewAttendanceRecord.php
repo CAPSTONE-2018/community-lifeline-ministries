838d18e236c242a7463dca37db7a8806a0a366de
joeyarbrough <jcyarbro88@gmail.com>
2018-03-09 15:25:36 -0600

    added submit button to bottom of attendance

--- php-files/new/NewAttendanceRecord.php
+++ php-files/new/NewAttendanceRecord.php
@@ -15,53 +15,58 @@ $getInfo = mysqli_fetch_array($currentStudentsInProgram, MYSQLI_ASSOC);
 $programName = $getInfo['Program_Name'];
 ?>
 
-<link rel="stylesheet" href="../../css/attendance-table-styles.css" />
-<link rel="stylesheet" href="../../node_modules/bootstrap/dist/css/bootstrap.min.css">
+<link rel="stylesheet" href="../../css/attendance-table-styles.css"/>
+
+<div class="container-fluid">
+    <div class="form-group">
+        <div class="card">
+            <form class="form-horizontal" action="../add/AddAttendanceRecord.php" method="POST" name="newAttendanceRecordForm"
+                  id="newAttendanceRecordForm">
+                <div class="card-body">
+
+                    <div class="header">
+                        <?php
+                        echo "<h3 class=\"card-title\">$programName Attendance</h3>";
+                        ?>
+                    </div>
 
 
-<div class="attendance-container">
-    <div class="attendance-title">
-        <?php
-            echo "<h2>$programName Attendance</h2>";
-        ?>
-    </div>
+                    <div class="card-content">
+                        <div>
+                            <!--Table-->
+                            <table class="table table-hover table-responsive">
+                                <!--Table head-->
+                                <thead>
+                                <tr>
+                                    <th>#</th>
+                                    <th class="left-column-title">Student Name</th>
+                                    <th class="centered-column-title">Present</th>
+                                    <th class="centered-column-title">Absent</th>
+                                    <th class="centered-column-title">Tardy</th>
+                                </tr>
+                                </thead>
 
-    <div class="card">
-        <div>
-            <!--Table-->
-            <table class="table table-hover table-responsive">
-                <!--Table head-->
-                <thead>
-                <tr>
-                    <th>#</th>
-                    <th class="left-column-title">Student Name</th>
-                    <th class="centered-column-title">Present</th>
-                    <th class="centered-column-title">Absent</th>
-                    <th class="centered-column-title">Tardy</th>
-                </tr>
-                </thead>
-
-                <tbody>
-                <?php
-                while ($row = mysqli_fetch_array($currentStudentsInProgram, MYSQLI_ASSOC)) {
-                    $studentName = $row['First_Name'] . " " . $row['Last_Name'];
-                    $studentId = mysqli_fetch_array($row['Student_Id'], MYSQLI_ASSOC);
-
-                    echo "<tr><td>";
-                    echo "";
-                    echo "</td><td class='student-name-column'>";
-                    echo $studentName;
-                    echo "</td><td class='check-mark-column'>";
-                    echo "
+                                <tbody>
+                                <?php
+                                while ($row = mysqli_fetch_array($currentStudentsInProgram, MYSQLI_ASSOC)) {
+                                    $studentName = $row['First_Name'] . " " . $row['Last_Name'];
+                                    $studentId = mysqli_fetch_array($row['Student_Id'], MYSQLI_ASSOC);
+                                    echo "<tr><td>
+                                
+                               </td><td class='student-name-column'>
+                                $studentName
+                                </td><td class='check-mark-column'>
+                               
                     <section class=\"checkbox-wrapper\">
                         <form class=\"checkbox-form ac-checkmark\" autocomplete=\"off\">
                             <ul>
                                 <li><input id=\"present-check-box\" name=\"present\" type=\"checkbox\"><label for=\"cb6\"></label></li>
                             </ul>
                         </form>
-                    </section>";
-                    echo "</td><td class='check-mark-column'>";
-                    echo "
+                    </section>
+                    
+                    </td><td class='check-mark-column'>
+                                
                     <section>
 				        <form class=\"checkbox-form ac-checkbox ac-checkmark\" autocomplete=\"off\">
 					        <ul>
@@ -69,29 +74,37 @@ $programName = $getInfo['Program_Name'];
 					        </ul>
                         </form>
 			        </section>
-                ";
-                    echo "</td><td class='check-mark-column'>";
-                    echo "
+                    </td><td class='check-mark-column'>          
                     <section class=\"checkbox-wrapper\">
                         <form class=\"checkbox-form\" autocomplete=\"off\">
                             <ul>
                                 <li><input id=\"tardy-check-box\" name=\"tardy\" type=\"checkbox\"><label for=\"cb6\"></label></li>
                             </ul>
                         </form>
-                    </section>";
-                    echo "</td></tr>";
-                }
-                ?>
-                </tbody>
-            </table>
+                    </section>
+                    </td></tr>";
+                                }
+                                ?>
+                                </tbody>
+                            </table>
+                        </div>
+                    </div>
 
+                    <div class="card-footer">
+                        <div>
+                            <button id="buttonTrigger" type="button" class="btn btn-right btn-primary"
+                                    data-toggle="modal" data-target="#exampleModalCenter">
+                                Verify Info
+                            </button>
+                        </div>
+                    </div>
+                </div>
+
+
+            </form>
         </div>
-
     </div>
-
 </div>
-
-
 
 
 <script type="text/javascript">
@@ -100,7 +113,7 @@ $programName = $getInfo['Program_Name'];
         text = 'textContent' in document ? 'textContent' : 'innerText';
     console.log(text);
 
-    for (var i = 1, len = rows.length; i < len; i++){
+    for (var i = 1, len = rows.length; i < len; i++) {
         rows[i].children[0][text] = i + ".";
     }
 </script>
