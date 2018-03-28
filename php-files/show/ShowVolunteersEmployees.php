e77ca97be7d632a31fb2bb7a3404d488e86a0595
nmacroy <nmacroy@gmail.com>
2018-03-27 19:40:36 -0500

    Added Search bar to each Show page

--- php-files/show/ShowVolunteersEmployees.php
+++ php-files/show/ShowVolunteersEmployees.php
@@ -8,13 +8,51 @@ include("../../db/config.php");
 $query = "SELECT * FROM Volunteer_Employees;";
 $result = mysqli_query($db, $query);
 ?>
+<script>
+    function FilterFields() {
+        // Declare variables
+        var input, filter, table, tr, td, i, show = false;
+        input = document.getElementById("searchInput");
+        filter = input.value.toUpperCase();
+        table = document.getElementById("volunteerTable");
+        tr = table.getElementsByTagName("tr");
 
+        // Loop through all table rows, and hide those who don't match the search query
+        for (i = 0; i < tr.length; i++) {
+            show = false;
+            for (j = 0; j < 14; j++) {
+                td = tr[i].getElementsByTagName("td")[j];
+                if (td) {
+                    if (td.innerHTML.toUpperCase().indexOf(filter) > -1) {
+                        tr[i].style.display = "";
+                        show = true;
+                    }
+                }
+            }
+            if(!show){
+                tr[i].style.display = "none";
+            }
+        }
+    }
+</script>
+<link rel="stylesheet" href="../../css/form-styles.css"/>
+<link rel="stylesheet" href="../../css/toggle-switch.css"/>
+<link rel="stylesheet" href="../../css/input-stylings.css"/>
+<link rel="stylesheet" href="../../css/new-toggle.css"/>
+<script defer src="https://code.getmdl.io/1.3.0/material.min.js"></script>
 <h1>Displaying All Volunteer/Employee:</h1>
+<div class="col-lg">
+
+    <div class="mdl-textfield mdl-js-textfield mdl-textfield--floating-label">
+        <input id="searchInput" class="mdl-textfield__input" name="searchInput" onkeyup="return FilterFields();" type="text"/>
+        <label class="mdl-textfield__label" for="searchInput">Search Volunteers/Employees</label>
+    </div>
+</div>
 <br/>
 
 <div id="print_div">
     <!--<link rel="stylesheet" type="text/css" href="DisplayAll.css"/>-->
-    <table class="table table-condensed table-striped">
+    <table id ="volunteerTable" class="table table-condensed table-striped">
         <thead>
         <tr>
             <th>ID</th>
@@ -23,15 +61,15 @@ $result = mysqli_query($db, $query);
             <th>Last Name</th>
             <th>Middle Name</th>
             <th>Suffix</th>
-            <th>Cell Phone</th>
-            <th>Home Phone</th>
-            <th>Address</th>
+            <th>Primary Phone</th>
+            <th>Secondary Phone</th>
+            <th>Address One</th>
+            <th>Address Two</th>
             <th>City</th>
             <th>State</th>
             <th>Zip</th>
             <th>Email</th>
             <th>Type</th>
-            <th>Program</th>
         </tr>
         </thead>
         <tbody>
@@ -50,11 +88,13 @@ $result = mysqli_query($db, $query);
             echo "</td><td>";
             echo $row['Suffix'];
             echo "</td><td>";
-            echo $row['Phone_Cell'];
+            echo $row['Primary_Phone'];
             echo "</td><td>";
-            echo $row['Phone_Home'];
+            echo $row['Secondary_Phone'];
             echo "</td><td>";
-            echo $row['Address'];
+            echo $row['Address_One'];
+            echo "</td><td>";
+            echo $row['Address_Two'];
             echo "</td><td>";
             echo $row['City'];
             echo "</td><td>";
@@ -64,7 +104,7 @@ $result = mysqli_query($db, $query);
             echo "</td><td>";
             echo $row['Email'];
             echo "</td><td>";
-            echo $row['Type'];
+            echo $row['TYPE'];
             echo "</td><td>";
         }
         ?>
