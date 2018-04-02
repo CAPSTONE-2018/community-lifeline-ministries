4ce57ce6887be323a0a28d2a7a77eef9382f57e3
joeyarbrough <jcyarbro88@gmail.com>
2018-04-02 09:32:15 -0500

    reformatted states

--- php-files/scripts/States.php
+++ php-files/scripts/States.php
@@ -6,87 +6,91 @@
  * @uses check_select
  * @return string
  */
-function stateDropdown($optionName ,$selected="IL") {
-	$states = array(
-		array('AL', 'Alabama'),
-    array('AK', 'Alaska'),
-		array('AR', 'Arkansas'),
-		array('AZ', 'Arizona'),
-		array('CA', 'California'),
-		array('CO', 'Colorado'),
-		array('CT', 'Connecticut'),
-		array('DC', 'District of Columbia'),
-		array('DE', 'Delaware'),
-		array('FL', 'Florida'),
-		array('GA', 'Georgia'),
-		array('HI', 'Hawaii'),
-		array('ID', 'Idaho'),
-		array('IL', 'Illinois'),
-		array('IN', 'Indiana'),
-    array('IA', 'Iowa'),
-		array('KS', 'Kansas'),
-		array('KY', 'Kentucky'),
-		array('LA', 'Louisiana'),
-    array('ME', 'Maine'),
-    array('MD', 'Maryland'),
-		array('MA', 'Massachusetts'),
-		array('MI', 'Michigan'),
-		array('MN', 'Minnesota'),
-		array('MS', 'Mississippi'),
-    array('MO', 'Missouri'),
-		array('MT', 'Montana'),
-		array('NC', 'North Carolina'),
-		array('ND', 'North Dakota'),
-		array('NE', 'Nebraska'),
-    array('NV', 'Nevada'),
-		array('NH', 'New Hampshire'),
-		array('NJ', 'New Jersey'),
-		array('NM', 'New Mexico'),
-		array('NY', 'New York'),
-		array('OH', 'Ohio'),
-		array('OK', 'Oklahoma'),
-		array('OR', 'Oregon'),
-		array('PA', 'Pennsylvania'),
-		array('PR', 'Puerto Rico'),
-		array('RI', 'Rhode Island'),
-		array('SC', 'South Carolina'),
-		array('SD', 'South Dakota'),
-		array('TN', 'Tennessee'),
-		array('TX', 'Texas'),
-		array('UT', 'Utah'),
-		array('VA', 'Virginia'),
-		array('VT', 'Vermont'),
-		array('WA', 'Washington'),
-		array('WI', 'Wisconsin'),
-		array('WV', 'West Virginia'),
-		array('WY', 'Wyoming')
-	);
+function stateDropdown($optionName, $selected = "IL")
+{
+    $states = array(
+        array('AL', 'Alabama'),
+        array('AK', 'Alaska'),
+        array('AR', 'Arkansas'),
+        array('AZ', 'Arizona'),
+        array('CA', 'California'),
+        array('CO', 'Colorado'),
+        array('CT', 'Connecticut'),
+        array('DC', 'District of Columbia'),
+        array('DE', 'Delaware'),
+        array('FL', 'Florida'),
+        array('GA', 'Georgia'),
+        array('HI', 'Hawaii'),
+        array('ID', 'Idaho'),
+        array('IL', 'Illinois'),
+        array('IN', 'Indiana'),
+        array('IA', 'Iowa'),
+        array('KS', 'Kansas'),
+        array('KY', 'Kentucky'),
+        array('LA', 'Louisiana'),
+        array('ME', 'Maine'),
+        array('MD', 'Maryland'),
+        array('MA', 'Massachusetts'),
+        array('MI', 'Michigan'),
+        array('MN', 'Minnesota'),
+        array('MS', 'Mississippi'),
+        array('MO', 'Missouri'),
+        array('MT', 'Montana'),
+        array('NC', 'North Carolina'),
+        array('ND', 'North Dakota'),
+        array('NE', 'Nebraska'),
+        array('NV', 'Nevada'),
+        array('NH', 'New Hampshire'),
+        array('NJ', 'New Jersey'),
+        array('NM', 'New Mexico'),
+        array('NY', 'New York'),
+        array('OH', 'Ohio'),
+        array('OK', 'Oklahoma'),
+        array('OR', 'Oregon'),
+        array('PA', 'Pennsylvania'),
+        array('PR', 'Puerto Rico'),
+        array('RI', 'Rhode Island'),
+        array('SC', 'South Carolina'),
+        array('SD', 'South Dakota'),
+        array('TN', 'Tennessee'),
+        array('TX', 'Texas'),
+        array('UT', 'Utah'),
+        array('VA', 'Virginia'),
+        array('VT', 'Vermont'),
+        array('WA', 'Washington'),
+        array('WI', 'Wisconsin'),
+        array('WV', 'West Virginia'),
+        array('WY', 'Wyoming')
+    );
 
-	$options = '<ul for="state" id="state" class="mdl-menu mdl-menu--bottom-left mdl-js-menu" name="'.$optionName.'">';
+    $options = '<ul for="state" id="state" class="mdl-menu mdl-menu--bottom-left mdl-js-menu" name="' . $optionName . '">';
 
-	foreach ($states as $state) {
-    	$options .= '<li class="mdl-menu__item" value="'.$state[0].'" '. check_select($selected, $state[0]) .' >'.$state[1].'</li>';
-  }
+    foreach ($states as $state) {
+        $options .= '<li class="mdl-menu__item" value="' . $state[0] . '" ' . check_select($selected, $state[0]) . ' >' . $state[1] . '</li>';
+    }
 
-  $options .= '</ul>';
-  return $options;
+    $options .= '</ul>';
+    return $options;
 
 }
 
 /**
  * Check Select Element
  *
- * @param string $i, POST value
- * @param string $m, input element's value
- * @param string $e, return=false, echo=true
+ * @param $selected
+ * @param $state
  * @return string
+ * @internal param string $i , POST value
+ * @internal param string $m , input element's value
+ * @internal param string $e , return=false, echo=true
  */
-function check_select($selected,$state) {
-		if ( $selected == $state) {
-			$val = ' selected="selected" ';
-		}else {
-			$val = '';
-		}
+function check_select($selected, $state)
+{
+    if ($selected == $state) {
+        $val = ' selected="selected" ';
+    } else {
+        $val = '';
+    }
 
-		return $val;
+    return $val;
 }
