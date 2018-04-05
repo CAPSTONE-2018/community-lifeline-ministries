10eb91c150169861d341bb5c342a3961573d953d
joeyarbrough <jcyarbro88@gmail.com>
2018-04-05 11:14:07 -0500

    added green check mark, or red circle depending on if student has all documents

--- php-files/show/ShowStudents.php
+++ php-files/show/ShowStudents.php
@@ -39,7 +39,7 @@ $dynamicRowId = 0;
                                     <th class="col-sm-1">#</th>
                                     <th class="col-sm-3">Student Name</th>
                                     <th class="col-sm-2 text-center">Programs</th>
-                                    <th class="col-sm-1 text-center">All Docs On File</th>
+                                    <th class="col-sm-1 text-center">Documents</th>
                                     <th class="col-sm-5 text-center">Actions</th>
                                 </tr>
                                 </thead>
@@ -49,6 +49,7 @@ $dynamicRowId = 0;
                                 while ($activeStudentsRow = mysqli_fetch_array($activeStudentResults, MYSQLI_ASSOC)) {
                                     $numberOfProgramsRow = mysqli_fetch_array($enrolledProgramResults, MYSQLI_ASSOC);
 
+                                    $studentDocumentFlag = false;
                                     $dynamicRowId++;
                                     $studentIdToSearch = $activeStudentsRow['Id'];
                                     $studentName = $activeStudentsRow['First_Name'] . " " . $activeStudentsRow['Last_Name'];
@@ -60,10 +61,23 @@ $dynamicRowId = 0;
                                         <input type='hidden' name='studentId[$dynamicRowId]' value=$studentIdToSearch />
                                         <input type='hidden' name='programId[$dynamicRowId]' value=$programId />
                                     </td>                                    
-                                    <td class='col-sm-2 align-middle text-center text-align-middle'>$numberOfPrograms</td>
+                                    <td class='col-sm-2 align-middle text-center text-align-middle'>$numberOfPrograms</td>";
 
-                                    <td class='col-sm-1 align-middle text-center'></td>
-                                    
+                                    if(($activeStudentsRow['Permission_Slip'] == 1) &&
+                                        ($activeStudentsRow['Birth_Certificate'] == 1) &&
+                                        ($activeStudentsRow['Reduced_Lunch_Eligible'] == 1) &&
+                                        ($activeStudentsRow['IEP'] == 1)) {
+                                        echo "<td class='col-sm-1 align-middle text-center'>
+                                                <i class='green-check fa fa-check-square-o'></i>
+                                            </td>";
+                                    } else {
+                                        echo "
+                                            <td class='col-sm-1 align-middle text-center'>
+                                                <a data-toggle='collapse' data-target='.collapseDocumentsRow$dynamicRowId'><i class='red-circle fa fa-ban'></i></a>
+                                            </td>";
+                                    }
+
+                                    echo "
                                     <td class='col-5'>
                                         <div class='left-action-buttons-container d-inline m-auto'>
                                            <div class=' d-inline'>
@@ -73,25 +87,46 @@ $dynamicRowId = 0;
                                             <div class='d-inline'>
                                                 <a href='#'><button type='button' id='deleteStudentButton' class='btn large-action-buttons delete-student-button'><i class='fa fa-trash-o'></i> Delete</button></a>
                                             </div>
-                                            </div>
+                                        </div>
                                             
                                             <div class='right-action-buttons-container d-inline'>
-                                            
-                                                <span title='Student Contacts' data-toggle='tooltip' class='small-action-buttons'>
-                                                    <button type='button'  id='studentContactButton' class='btn small-action-buttons student-contact-button' data-toggle='collapse' data-target='.collapseContactRow$dynamicRowId'><i class='fa fa-phone'></i></button>
-                                                </span>
-                                                
-                                                <span title='Student Allergies' data-toggle='tooltip' class='small-action-buttons'>
-                                                    <button type='button' id='studentAllergiesButton' class='btn small-action-buttons view-allergies-button' data-toggle='collapse' data-target='.collapseAllergyRow$dynamicRowId'><i class='fa fa-bullhorn'></i></button>
-                                                </span>
-                                                
                                                 <span title='Test Scores' data-toggle='tooltip' class='small-action-buttons'>
                                                     <button type='button' id='studentTestScoresButton' class='btn small-action-buttons test-scores-button' data-toggle='collapse' data-target='.collapseTestScoresRow$dynamicRowId'><i class='fa fa-bar-chart'></i></button>
                                                 </span>
+                                                <span title='Student Allergies' data-toggle='tooltip' class='small-action-buttons'>
+                                                    <button type='button' id='studentAllergiesButton' class='btn small-action-buttons view-allergies-button' data-toggle='collapse' data-target='.collapseAllergyRow$dynamicRowId'><i class='fa fa-bullhorn'></i></button>
+                                                </span>
+                                                <span title='Student Contacts' data-toggle='tooltip' class='small-action-buttons'>
+                                                    <button type='button'  id='studentContactButton' class='btn small-action-buttons student-contact-button' data-toggle='collapse' data-target='.collapseContactRow$dynamicRowId'><i class='fa fa-phone'></i></button>
+                                                </span>
                                             </div>
-                                        </div>
+                                        
                                     </td>
                                 </tr>";
+                                    $queryForDocuments = "SELECT Permission_Slip, Birth_Certificate, Reduced_Lunch_Eligible, IEP FROM Students WHERE Id = $studentIdToSearch;";
+                                    $documentResults = mysqli_query($db, $queryForDocuments);
+                                    while ($documentsRow = mysqli_fetch_array($documentResults, MYSQLI_ASSOC)){
+
+                                        echo "
+                                        <tr class='collapse smooth collapseDocumentsRow$dynamicRowId'>
+                                            <td></td>
+                                            <td colspan='12'>
+
+                                                <span>";
+                                                    if ($documentsRow['Permission_Slip'] == 0) {
+                                                        echo "No Permission Slip.  ";
+                                                    }
+                                                    if ($documentsRow['Birth_Certificate'] == 0) {
+                                                        echo "  No Birth Certificate.";
+                                                    }
+                                                    if ($documentsRow['Reduced_Lunch_Eligible'] == 0) {
+                                                        echo " Reduced Lunch";
+                                                    }
+
+                                                "</span>
+                                            </td>
+                                        </tr>";
+                                    }
 
                                     $queryForContacts = "SELECT Contacts.First_Name, Contacts.Last_Name, Contacts.Primary_Phone
                                   FROM Student_To_Contacts JOIN Contacts On Student_To_Contacts.Contact_Id = Contacts.Id WHERE Student_Id = $studentIdToSearch;";
@@ -100,30 +135,31 @@ $dynamicRowId = 0;
                                         $contactName = $contactRow['First_Name'] . " " . $contactRow['Last_Name'];
                                         $contactPhone = $contactRow['Primary_Phone'];
                                         echo "
-                                    <tr class='collapse smooth collapseContactRow$dynamicRowId'>
-                                        <td></td>
-                                        <td colspan='12'>
-                                            <span class='hidden-row-width'><i class='glyphicon glyphicon-user'></i> $contactName</span>
-                                            <span class='hidden-row-width'><i class='glyphicon glyphicon-earphone'></i> $contactPhone</span>
-                                        </td>
-                                    </tr>";
+                                            <tr class='collapse smooth collapseContactRow$dynamicRowId'>
+                                                <td></td>
+                                                <td colspan='12'>
+                                                <span class='hidden-row-width'><i class='glyphicon glyphicon-user'></i> $contactName</span>
+                                                <span class='hidden-row-width'><i class='glyphicon glyphicon-earphone'></i> $contactPhone</span>
+                                                </td>
+                                            </tr>";
 
                                         $queryForStudentAllergies = "SELECT Medical_Concerns.Name, Medical_Concern_Types.Type  FROM
                                                                       (Medical_Concerns JOIN Student_To_Medical_Concerns ON Medical_Concerns.Id = Student_To_Medical_Concerns.Medical_Concern_Id)
                                                                       JOIN Medical_Concern_Types ON Medical_Concern_Types.Id = Student_To_Medical_Concerns.Medical_Type_Id WHERE Student_Id = $studentIdToSearch;";
                                         $studentAllergies = mysqli_query($db, $queryForStudentAllergies);
-                                        while($allergyRow = mysqli_fetch_array($studentAllergies, MYSQLI_ASSOC)) {
+                                        while ($allergyRow = mysqli_fetch_array($studentAllergies, MYSQLI_ASSOC)) {
 
                                             $allergyName = $allergyRow['Name'];
                                             $allergyType = $allergyRow['Type'];
 
-                                            echo "<tr class='collapse smooth collapseAllergyRow$dynamicRowId'>
+                                            echo "
+                                                <tr class='collapse smooth collapseAllergyRow$dynamicRowId'>
                                                     <td></td>
                                                     <td colspan='12'>
                                                         <span class='hidden-row-width'><i class='fa fa-bullhorn'></i> $allergyName</span>
                                                         <span class='hidden-row-width'>$allergyType</span>
                                                     </td>
-                                                  </tr>";
+                                                </tr>";
                                         }
                                     }
                                 }?>
