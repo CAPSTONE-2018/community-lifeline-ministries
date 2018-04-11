7f9778b53d2648e34dbccdf27373e3f5058eb28d
joeyarbrough <jcyarbro88@gmail.com>
2018-04-11 14:03:04 -0500

    removed stylesheet links from ShowVolunteers

--- php-files/show/ShowVolunteersEmployees.php
+++ php-files/show/ShowVolunteersEmployees.php
@@ -8,11 +8,6 @@ include("../../db/config.php");
 $query = "SELECT * FROM Volunteer_Employees;";
 $result = mysqli_query($db, $query);
 ?>
-<script type="text/javascript" src="../../js/FilterFields.js"></script>
-<link rel="stylesheet" href="../../css/form-styles.css"/>
-<link rel="stylesheet" href="../../css/toggle-switch.css"/>
-<link rel="stylesheet" href="../../css/input-styles.css"/>
-<script defer src="https://code.getmdl.io/1.3.0/material.min.js"></script>
 <h1>Displaying All Volunteer/Employee:</h1>
 <div class="col-lg">
 
