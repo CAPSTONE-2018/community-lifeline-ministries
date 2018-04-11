68d29c13ab5eddf9e9238f17f2ee1c86cc9958f2
joeyarbrough <jcyarbro88@gmail.com>
2018-04-11 14:04:11 -0500

    removed pretty dropdowns from menu

--- php-files/index-login/menu.php
+++ php-files/index-login/menu.php
@@ -5,17 +5,3 @@ include("../menu/AttendanceCard.php");
 
 include("../scripts/footer.php");
 ?>
-
-
-<script defer src="../../js/pretty-dropdowns.js"></script>
-<script type="text/javascript">
-    $(document).ready(function () {
-        $dropdown = $('select').prettyDropdown({
-            height: 40,
-            classic: true,
-            customClass: "triangle on-hover"
-        });
-    });
-    // When <select> state changes...
-    $dropdown.refresh();
-</script>
