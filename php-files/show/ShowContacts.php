3e55bac87d46e8ae87aa4142b1ecb7c46ccfc865
joeyarbrough <jcyarbro88@gmail.com>
2018-04-11 14:01:46 -0500

    removed stylesheet links from ShowContacts

--- php-files/show/ShowContacts.php
+++ php-files/show/ShowContacts.php
@@ -9,12 +9,6 @@ include("../../db/config.php");
 $query = "SELECT * FROM Contacts;";
 $result = mysqli_query($db, $query);
 ?>
-<script type="text/javascript" src="../../js/FilterFields.js"></script>
-<link rel="stylesheet" href="../../css/form-styles.css"/>
-<link rel="stylesheet" href="../../css/toggle-switch.css"/>
-<link rel="stylesheet" href="../../css/input-styles.css"/>
-<link rel="stylesheet" href="../../css/new-toggle.css"/>
-<script defer src="https://code.getmdl.io/1.3.0/material.min.js"></script>
 <h1>Displaying All Contacts:</h1>
 <div class="col-lg">
 
