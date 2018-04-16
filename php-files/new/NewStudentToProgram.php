fb7ff2e6b0a1e364e9765e9a0d7a47ce3fcb806b
joeyarbrough <jcyarbro88@gmail.com>
2018-04-16 13:34:51 -0500

    new student to program file is now wrapped and bootstrapped up

--- php-files/new/NewStudentToProgram.php
+++ php-files/new/NewStudentToProgram.php
@@ -5,47 +5,67 @@ include("../scripts/header.php");
 //connect to database
 include("../../db/config.php");
 
-$query = "SELECT * FROM Students ORDER BY Last_Name, First_Name;";
-$result = mysqli_query($db, $query);
+$queryForAllActiveStudents = "SELECT * FROM Students WHERE Active_Student = 1 ORDER BY Last_Name, First_Name;";
+$allActiveStudentsResults = mysqli_query($db, $queryForAllActiveStudents);
 
-$query2 = "SELECT * FROM Programs ORDER BY Program_Name";
-$result2 = mysqli_query($db, $query2);
+$queryForAllActivePrograms = "SELECT * FROM Programs WHERE Active_Program = 1 ORDER BY Program_Name";
+$allActiveProgramsResults = mysqli_query($db, $queryForAllActivePrograms);
 ?>
-
-<div id="form_wrapper">
-    <form class="form-horizontal" action="../add/AddStudentToProgram.php" method="POST" id="form2">
-
-        <h1>Add Student and Program Information:</h1>
-        <br/>
-        <div class="row">
-            <div class="form-group">
-                <div class="col-lg-6">
-                    <label class="control-label" for="studentId">Student Name:</label>
-                    <select id="studentId" class="form-control" name="studentId">
-                        <?php
-                        if (mysqli_num_rows($result) > 0) {
-                            while ($row = mysqli_fetch_assoc($result)) {
-                                echo "<option value='" . $row['Id'] . "'>" . $row['First_Name'] . " " . $row['Last_Name'] . "</option>";
-                            }
-                        }
-                        ?>
-                    </select>
-                </div>
-                <div class="col-lg-6">
-                    <label class="control-label" for="programId">Program:</label>
-                    <select id="programId" class="form-control" name="programId">
-                        <?php
-                        if (mysqli_num_rows($result2) > 0) {
-                            while ($row = mysqli_fetch_assoc($result2)) {
-                                echo "<option value='" . $row['Id'] . "'>" . $row['Program_Name'] . "</option>";
-                            }
-                        }
-                        ?>
-                    </select>
-                </div>
-            </div>
+<div class="container-fluid col-sm-10">
+    <div class="card text-center">
+        <div class="card-header">
+            <h1><i class="fa fa-pencil"></i> Register Student to Program</h1>
         </div>
-        <input id="submit" class="btn btn-primary btn-lg btn-block" type="submit" value="Submit">
-    </form>
+
+        <div class="card-body">
+            <form action="../add/AddStudentToProgram.php" method="post" id="newStudentToProgramForm">
+                <div class="form-group">
+                    <div class="col-sm-6 ">
+                        <div class="mdl-textfield mdl-js-textfield mdl-textfield--floating-label getmdl-select">
+                            <input type="text" class="mdl-textfield__input" readonly>
+                            <input type="hidden" id="studentId" name="studentId">
+                            <i class="mdl-icon-toggle__label fa fa-angle-down"></i>
+                            <label for="studentId" class="mdl-textfield__label">Select Student</label>
+                            <ul for="studentId" class="mdl-menu mdl-menu--bottom-left mdl-js-menu">
+                                <?php while ($studentRow = mysqli_fetch_assoc($allActiveStudentsResults)) {
+                                    $studentName = $studentRow['First_Name'] . ' ' . $studentRow['Last_Name'];
+                                    ?>
+                                    <li class="mdl-menu__item"
+                                        data-val="<?php echo $studentRow['Id']; ?>"><?php echo $studentName; ?></li>
+
+                                <?php } ?>
+
+                            </ul>
+                        </div>
+                    </div>
+
+                    <div class="col-sm-6 ">
+                        <div class="mdl-textfield mdl-js-textfield mdl-textfield--floating-label getmdl-select">
+                            <input type="text" class="mdl-textfield__input" readonly>
+                            <input type="hidden" id="programId" name="programId">
+                            <i class="mdl-icon-toggle__label fa fa-angle-down"></i>
+                            <label for="programId" class="mdl-textfield__label">Program to Enroll</label>
+                            <ul for="programId" class="mdl-menu mdl-menu--bottom-left mdl-js-menu">
+                                <?php while ($programsRow = mysqli_fetch_assoc($allActiveProgramsResults)) {
+                                    $programName = $programsRow['Program_Name'];
+                                    ?>
+                                    <li class="mdl-menu__item"
+                                        data-val="<?php echo $programsRow['Id']; ?>"><?php echo $programName; ?></li>
+
+                                <?php } ?>
+
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="row col-sm-12">
+                    <input id="submitButton" class="btn large-action-buttons edit-student-button"
+                           form="newStudentToProgramForm" type="submit" value="Enter"><br><br>
+                </div>
+            </form>
+        </div>
+    </div>
 </div>
+
 <?php include("../scripts/footer.php"); ?>
