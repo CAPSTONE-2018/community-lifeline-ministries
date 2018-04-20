3f2b26990a4b7923b259daa2fc9d85b18ad69369
joeyarbrough <jcyarbro88@gmail.com>
2018-04-20 01:09:59 -0500

    now the branch should be good

--- js/modals/new-student/AddStudentConfirmation.js
+++ js/modals/new-student/AddStudentConfirmation.js
@@ -14,14 +14,14 @@ function launchStudentConfirmationModal(serializedForm) {
             var homePageButtonTitle = document.createTextNode("Back to Home Page");
             homePageButton.appendChild(homePageButtonTitle);
             homePageButton.onclick = function () {
-                location.href = "../../php-files/index-login/menu.php";
+                location.href = "../../../php-files/index-login/menu.php";
             };
 
             var newStudentButton = document.createElement("BUTTON");
             var newStudentButtonTitle = document.createTextNode("Add New Student");
             newStudentButton.appendChild(newStudentButtonTitle);
             newStudentButton.onclick = function () {
-                location.href = "../../php-files/new/NewStudent.php";
+                location.href = "../../../php-files/new/NewStudent.php";
             };
 
             var footerRow = '<div id="footerButtons" class="text-center">' +
