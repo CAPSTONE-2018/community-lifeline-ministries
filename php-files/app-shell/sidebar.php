4708fe2275cfbd0c0fa04ad4ed141c7587b6da94
Lars Knutson II <larsknutson@lewisu.edu>
2018-04-30 12:05:03 -0500

    new left sidebar now has all the right links

--- php-files/app-shell/sidebar.php
+++ php-files/app-shell/sidebar.php
@@ -1,76 +1,206 @@
 <!-- Sidebar menu-->
 <div class="app-sidebar__overlay" data-toggle="sidebar"></div>
 <aside class="app-sidebar">
-    <div class="app-sidebar__user"><img class="app-sidebar__user-avatar"
-                                        src="https://s3.amazonaws.com/uifaces/faces/twitter/jsa/48.jpg"
-                                        alt="User Image">
-        <div>
-            <p class="app-sidebar__user-name">John Doe</p>
-            <p class="app-sidebar__user-designation">Frontend Developer</p>
-        </div>
+    <div class="app-sidebar__user">
+        <img class="app-sidebar__user-avatar"
+             src="../../images/new-logo.png"
+             alt="CLM Logo"/>
     </div>
     <ul class="app-menu">
-        <li><a class="app-menu__item active" href="index.html"><i class="app-menu__icon fa fa-dashboard"></i><span
-                    class="app-menu__label">Dashboard</span></a></li>
-        <li class="treeview"><a class="app-menu__item" href="#" data-toggle="treeview"><i
-                    class="app-menu__icon fa fa-laptop"></i><span class="app-menu__label">UI Elements</span><i
-                    class="treeview-indicator fa fa-angle-right"></i></a>
+        <li><a class="app-menu__item active" href="#">
+                <i class="app-menu__icon fa fa-dashboard"></i>
+                <span class="app-menu__label">Dashboard</span>
+            </a>
+        </li>
+        <li class="treeview">
+            <a class="app-menu__item" href="#" data-toggle="treeview">
+                <i class="app-menu__icon fa fa-graduation-cap"></i>
+                <span class="app-menu__label"> Students</span>
+                <i class="treeview-indicator fa fa-angle-right"></i>
+            </a>
             <ul class="treeview-menu">
-                <li><a class="treeview-item" href="bootstrap-components.html"><i class="icon fa fa-circle-o"></i>
-                        Bootstrap Elements</a></li>
-                <li><a class="treeview-item" href="https://fontawesome.com/v4.7.0/icons/" target="_blank"
-                       rel="noopener"><i class="icon fa fa-circle-o"></i> Font Icons</a></li>
-                <li><a class="treeview-item" href="ui-cards.html"><i class="icon fa fa-circle-o"></i> Cards</a></li>
-                <li><a class="treeview-item" href="widgets.html"><i class="icon fa fa-circle-o"></i> Widgets</a></li>
+                <li>
+                    <a class="treeview-item" href="../new/NewStudent.php">
+                        <i class="icon fa fa-plus"></i>
+                        Add New Student
+                    </a>
+                </li>
+                <li>
+                    <a class="treeview-item" href="../show/ShowStudents.php">
+                        <i class="icon fa fa-folder-open"></i>
+                        View All
+                    </a>
+                </li>
+                <li>
+                    <a class="treeview-item" href="../new/NewStudentToProgram.php">
+                        <i class="icon fa fa-pencil-square-o"></i>
+                        Register In Program
+                    </a>
+                </li>
             </ul>
         </li>
-        <li><a class="app-menu__item" href="charts.html"><i class="app-menu__icon fa fa-pie-chart"></i><span
-                    class="app-menu__label">Charts</span></a></li>
-        <li class="treeview"><a class="app-menu__item" href="#" data-toggle="treeview"><i
-                    class="app-menu__icon fa fa-edit"></i><span class="app-menu__label">Forms</span><i
-                    class="treeview-indicator fa fa-angle-right"></i></a>
+        <li class="treeview">
+            <a class="app-menu__item" href="#" data-toggle="treeview">
+                <i class="app-menu__icon fa fa-address-book-o"></i>
+                <span class="app-menu__label"> Contacts</span>
+                <i class="treeview-indicator fa fa-angle-right"></i>
+            </a>
             <ul class="treeview-menu">
-                <li><a class="treeview-item" href="form-components.html"><i class="icon fa fa-circle-o"></i> Form
-                        Components</a></li>
-                <li><a class="treeview-item" href="form-custom.html"><i class="icon fa fa-circle-o"></i> Custom
-                        Components</a></li>
-                <li><a class="treeview-item" href="form-samples.html"><i class="icon fa fa-circle-o"></i> Form
-                        Samples</a></li>
-                <li><a class="treeview-item" href="form-notifications.html"><i class="icon fa fa-circle-o"></i> Form
-                        Notifications</a></li>
+                <li>
+                    <a class="treeview-item" href="../new/NewContact.php">
+                        <i class="icon fa fa-plus"></i>
+                        Add New Contact
+                    </a>
+                </li>
+                <li>
+                    <a class="treeview-item" href="../show/ShowContacts.php">
+                        <i class="icon fa fa-folder-open"></i>
+                        View All
+                    </a>
+                </li>
+                <li>
+                    <a class="treeview-item" href="#">
+                        <i class="icon fa fa-pencil-square-o"></i>
+                        Add Student to Contact
+                    </a>
+                </li>
             </ul>
         </li>
-        <li class="treeview"><a class="app-menu__item" href="#" data-toggle="treeview"><i
-                    class="app-menu__icon fa fa-th-list"></i><span class="app-menu__label">Tables</span><i
-                    class="treeview-indicator fa fa-angle-right"></i></a>
+        <li class="treeview">
+            <a class="app-menu__item" href="#" data-toggle="treeview">
+                <i class="app-menu__icon fa fa-star"></i>
+                <span class="app-menu__label"> Volunteers</span>
+                <i class="treeview-indicator fa fa-angle-right"></i>
+            </a>
             <ul class="treeview-menu">
-                <li><a class="treeview-item" href="table-basic.html"><i class="icon fa fa-circle-o"></i> Basic
-                        Tables</a></li>
-                <li><a class="treeview-item" href="table-data-table.html"><i class="icon fa fa-circle-o"></i> Data
-                        Tables</a></li>
+                <li>
+                    <a class="treeview-item" href="../new/NewProgram.php">
+                        <i class="icon fa fa-plus"></i>
+                        Add New Volunteer
+                    </a>
+                </li>
+                <li>
+                    <a class="treeview-item" href="../show/ShowPrograms.php">
+                        <i class="icon fa fa-folder-open"></i>
+                        View All
+                    </a>
+                </li>
             </ul>
         </li>
-        <li class="treeview"><a class="app-menu__item" href="#" data-toggle="treeview"><i
-                    class="app-menu__icon fa fa-file-text"></i><span class="app-menu__label">Pages</span><i
-                    class="treeview-indicator fa fa-angle-right"></i></a>
+        <li class="treeview">
+            <a class="app-menu__item" href="#" data-toggle="treeview">
+                <i class="app-menu__icon fa fa-pencil"></i>
+                <span class="app-menu__label"> Programs</span>
+                <i class="treeview-indicator fa fa-angle-right"></i>
+            </a>
             <ul class="treeview-menu">
-                <li><a class="treeview-item" href="blank-page.html"><i class="icon fa fa-circle-o"></i> Blank Page</a>
+                <li>
+                    <a class="treeview-item" href="../new/NewProgram.php">
+                        <i class="icon fa fa-plus"></i>
+                        Add New Program
+                    </a>
                 </li>
-                <li><a class="treeview-item" href="page-login.html"><i class="icon fa fa-circle-o"></i> Login Page</a>
+                <li>
+                    <a class="treeview-item" href="../show/ShowPrograms.php">
+                        <i class="icon fa fa-folder-open"></i>
+                        View All
+                    </a>
                 </li>
-                <li><a class="treeview-item" href="page-lockscreen.html"><i class="icon fa fa-circle-o"></i> Lockscreen
-                        Page</a></li>
-                <li><a class="treeview-item" href="page-user.html"><i class="icon fa fa-circle-o"></i> User Page</a>
-                </li>
-                <li><a class="treeview-item" href="page-invoice.html"><i class="icon fa fa-circle-o"></i> Invoice
-                        Page</a></li>
-                <li><a class="treeview-item" href="page-calendar.html"><i class="icon fa fa-circle-o"></i> Calendar Page</a>
-                </li>
-                <li><a class="treeview-item" href="page-mailbox.html"><i class="icon fa fa-circle-o"></i> Mailbox</a>
-                </li>
-                <li><a class="treeview-item" href="page-error.html"><i class="icon fa fa-circle-o"></i> Error Page</a>
+                <li>
+                    <a class="treeview-item" href="#">
+                        <i class="icon fa fa-calendar-check-o"></i>
+                         Attendance
+                    </a>
                 </li>
             </ul>
+        </li>
+        <li class="treeview">
+            <a class="app-menu__item" href="#" data-toggle="treeview">
+                <i class="app-menu__icon fa fa-warning"></i>
+                <span class="app-menu__label"> Medical Concerns</span>
+                <i class="treeview-indicator fa fa-angle-right"></i>
+            </a>
+            <ul class="treeview-menu">
+                <li>
+                    <a class="treeview-item" href="../new/NewMedicalConcern.php">
+                        <i class="icon fa fa-plus"></i>
+                        Add New Concern
+                    </a>
+                </li>
+                <li>
+                    <a class="treeview-item" href="../show/ShowMedicalConcerns.php">
+                        <i class="icon fa fa-folder-open"></i>
+                        View All
+                    </a>
+                </li>
+                <li>
+                    <a class="treeview-item" href="#">
+                        <i class="icon fa fa-graduation-cap"></i>
+                         Add Student Concern
+                    </a>
+                </li>
+            </ul>
+        </li>
+        <li class="treeview">
+            <a class="app-menu__item" href="#" data-toggle="treeview">
+                <i class="app-menu__icon fa fa-bar-chart"></i>
+                <span class="app-menu__label"> Reports</span>
+                <i class="treeview-indicator fa fa-angle-right"></i>
+            </a>
+            <ul class="treeview-menu">
+                <li>
+                    <a class="treeview-item" href="../create/CreateStudentReport.php">
+                        Student Reports
+                    </a>
+                </li>
+                <li>
+                    <a class="treeview-item" href="../create/CreateContactReport.php">
+                        Contact Reports
+                    </a>
+                </li>
+                <li>
+                    <a class="treeview-item" href="../create/CreateVolunteerReport.php">
+                        Volunteer Reports
+                    </a>
+                </li>
+                <li>
+                    <a class="treeview-item" href="../create/CreateMedicalReport.php">
+                        Medical Reports
+                    </a>
+                </li>
+                <li>
+                    <a class="treeview-item" href="../create/CreateStudentMedicalReport.php">
+                        Student Medical Reports
+                    </a>
+                </li>
+                <li>
+                    <a class="treeview-item" href="../create/CreateStudentToProgramReport.php">
+                        Program Reports
+                    </a>
+                </li>
+                <li>
+                    <a class="treeview-item" href="../create/CreateAttendanceReport.php">
+                        Attendance Reports
+                    </a>
+                </li>
+                <li>
+                    <a class="treeview-item" href="../create/CreateStudentAttendanceReport.php">
+                        Student Attendance Reports
+                    </a>
+                </li>
+            </ul>
+        </li>
+        <li>
+            <a class="app-menu__item" href="../index-login/UserDocumentation.php">
+                <i class="app-menu__icon fa fa-question-circle"></i>
+                <span class="app-menu__label">Help</span>
+            </a>
+        </li>
+        <li>
+            <a class="app-menu__item" href="../index-login/logout.php">
+                <i class="app-menu__icon fa fa-power-off"></i>
+                <span class="app-menu__label">Logout</span>
+            </a>
         </li>
     </ul>
 </aside>
@@ -122,8 +252,6 @@
             </div>
         </div>
     </div>
-
-
     <div class="row">
         <div class="col-md-6">
             <div class="tile">
