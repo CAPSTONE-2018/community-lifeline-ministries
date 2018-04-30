bb39c1dbe803d40c7e42b4b159af2e41c4fcd99f
Lars Knutson II <larsknutson@lewisu.edu>
2018-04-30 14:26:00 -0500

    pulled stuff out of sidebar that will be in the dashboard.php file

--- php-files/index-login/main-menu.php
+++ php-files/index-login/main-menu.php
@@ -1,4 +1,6 @@
 <?php
 include("../app-shell/header.php");
 include("../app-shell/sidebar.php");
+include ("../index-login/dashboard.php");
+include ("./AttendanceCard.php");
 include("../app-shell/footer.php");
