d396f83b98ddd054bac60b41da7343bab7ab5700
Josh <joshjurss@gmail.com>
2018-05-05 15:17:25 -0500

    Removed excess

--- php-files/generate/GenerateStudentReport.php
+++ php-files/generate/GenerateStudentReport.php
@@ -14,64 +14,63 @@ $studentsResults = mysqli_query($db, "SELECT * FROM Students " . $searchFilters)
             <form method="POST" action="#" name="allStudentsTable"
                   id="allStudentsTable">
                 <div class="table-responsive">
-                    <table id="search-table" class="table table-striped table-condensed table-hover">
-                        <thead>
-                        <table class="table">
-                            <thead class="thead-dark">
-                            <tr>
-                                <th scope="col">First Name</th>
-                                <th scope="col">Last Name</th>
-                                <th scope="col">Address</th>
-                                <th scope="col">City</th>
-                                <th scope="col">State</th>
-                                <th scope="col">Zip</th>
-                                <th scope="col">Ethnicity</th>
-                                <th scope="col">Gender</th>
-                                <th scope="col">Birth Date</th>
-                                <th scope="col">School</th>
-                                <th scope="col">Permission Slip</th>
-                                <th scope="col">Birth Certificate</th>
-                                <th scope="col">Reduced Lunch Eligible</th>
-                                <th scope="col">IEP</th>
-                            </tr>
-                            </thead>
-                            <tbody>
-                            <?php
-                            while ($row = mysqli_fetch_array($studentsResults, MYSQLI_ASSOC)) {
-                                echo "<tr scope=\"row\"><td>";
-                                echo $row['First_Name'];
-                                echo "</td><td>";
-                                echo $row['Last_Name'];
-                                echo "</td><td>";
-                                echo $row['Address_One'];
-                                echo "</br> " . $row['Address_Two'];
-                                echo "</td><td>";
-                                echo $row['City'];
-                                echo "</td><td>";
-                                echo $row['State'];
-                                echo "</td><td>";
-                                echo $row['Zip'];
-                                echo "</td><td>";
-                                echo $row['Ethnicity'];
-                                echo "</td><td>";
-                                echo $row['Gender'];
-                                echo "</td><td>";
-                                echo $row['Birth_Date'];
-                                echo "</td><td>";
-                                echo $row['School'];
-                                echo "</td><td>";
-                                echo $row['Permission_Slip'];
-                                echo "</td><td>";
-                                echo $row['Birth_Certificate'];
-                                echo "</td><td>";
-                                echo $row['Reduced_Lunch_Eligible'];
-                                echo "</td><td>";
-                                echo $row['IEP'];
-                                echo "</td></tr>";
-                            }
-                            ?>
-                            </tbody>
-                        </table>
+                    <thead>
+                    <table class="table">
+                        <thead class="thead-dark">
+                        <tr>
+                            <th scope="col">First Name</th>
+                            <th scope="col">Last Name</th>
+                            <th scope="col">Address</th>
+                            <th scope="col">City</th>
+                            <th scope="col">State</th>
+                            <th scope="col">Zip</th>
+                            <th scope="col">Ethnicity</th>
+                            <th scope="col">Gender</th>
+                            <th scope="col">Birth Date</th>
+                            <th scope="col">School</th>
+                            <th scope="col">Permission Slip</th>
+                            <th scope="col">Birth Certificate</th>
+                            <th scope="col">Reduced Lunch Eligible</th>
+                            <th scope="col">IEP</th>
+                        </tr>
+                        </thead>
+                        <tbody>
+                        <?php
+                        while ($row = mysqli_fetch_array($studentsResults, MYSQLI_ASSOC)) {
+                            echo "<tr scope=\"row\"><td>";
+                            echo $row['First_Name'];
+                            echo "</td><td>";
+                            echo $row['Last_Name'];
+                            echo "</td><td>";
+                            echo $row['Address_One'];
+                            echo "</br> " . $row['Address_Two'];
+                            echo "</td><td>";
+                            echo $row['City'];
+                            echo "</td><td>";
+                            echo $row['State'];
+                            echo "</td><td>";
+                            echo $row['Zip'];
+                            echo "</td><td>";
+                            echo $row['Ethnicity'];
+                            echo "</td><td>";
+                            echo $row['Gender'];
+                            echo "</td><td>";
+                            echo $row['Birth_Date'];
+                            echo "</td><td>";
+                            echo $row['School'];
+                            echo "</td><td>";
+                            echo $row['Permission_Slip'];
+                            echo "</td><td>";
+                            echo $row['Birth_Certificate'];
+                            echo "</td><td>";
+                            echo $row['Reduced_Lunch_Eligible'];
+                            echo "</td><td>";
+                            echo $row['IEP'];
+                            echo "</td></tr>";
+                        }
+                        ?>
+                        </tbody>
+                    </table>
                 </div>
             </form>
         </div>
