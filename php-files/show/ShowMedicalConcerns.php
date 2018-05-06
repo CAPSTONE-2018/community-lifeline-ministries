3418b1e6875b5a7be6f31fbd5684a8a4c757b555
jcyarbro88 <jcyarbro88@gmail.com>
2018-05-06 00:18:19 -0500

    medical concerns table now reflects data tables library

--- php-files/show/ShowMedicalConcerns.php
+++ php-files/show/ShowMedicalConcerns.php
@@ -8,113 +8,105 @@ JOIN Student_To_Medical_Concerns ON Medical_Concern_Types.Id = Student_To_Medica
     JOIN Students ON Student_To_Medical_Concerns.Student_Id = Students.Id WHERE Medical_Concern_Types.Active_Id = 1;";
 $medicalConcernsResults = mysqli_query($db, $queryForMedicalConcerns);
 ?>
-    <div class="app-title">
-        <div>
-            <h3><i class="fa fa-bookmark-o"></i> Medical Concerns</h3>
-        </div>
-        <ul class="app-breadcrumb breadcrumb">
-            <li class="breadcrumb-item"><a href="../index-login/Main-Menu.php"><i class="fa fa-home fa-lg"></i></a></li>
-            <li class="breadcrumb-item"> Medical Concerns</li>
-        </ul>
+<div class="app-title">
+    <div>
+        <h3><i class="fa fa-bookmark-o"></i> Medical Concerns</h3>
     </div>
-    <div class="container-fluid">
-        <div class="card">
-            <div class="card-header">
-                <div class="col-12 text-center">
-                    <h2><i class="fa fa-warning"></i> All Medical Concern Types</h2>
-                </div>
+    <ul class="app-breadcrumb breadcrumb">
+        <li class="breadcrumb-item"><a href="../index-login/Main-Menu.php"><i class="fa fa-home fa-lg"></i></a></li>
+        <li class="breadcrumb-item"> Medical Concerns</li>
+    </ul>
+</div>
 
-                <div class="row">
-                    <div class="search-input-wrapper col-6">
-                        <input id="search-input" type="text" placeholder="Search" onkeyup="FilterFields()">
-                        <i class="align-middle fa fa-search fa-lg fa-fw" aria-hidden="true"></i>
-                    </div>
+<div class="card">
+    <div class="card-header">
+        <div class="col-12 text-center">
+            <h2><i class="fa fa-warning"></i> All Medical Concern Types</h2>
+        </div>
+    </div>
 
-                    <div class="align-middle m-auto text-right col-6">
-                        <div class="btn-group btn-toggle">
-                            <button class="btn btn-primary active" data-toggle="collapse" data-target="#collapsible2">
-                                Show All
-                            </button>
+    <div class="container">
+        <table id="showMedicalConcernsTable" class="table table-striped table-bordered table-hover" style="width:100%">
+            <thead>
+            <tr>
+                <th>#</th>
+                <th class="text-center">Medical Concerns</th>
+                <th class="text-center">Concern Type</th>
+                <th class="text-center">Students With Concern</th>
+                <th class="text-center">Actions</th>
+            </tr>
+            </thead>
+
+            <tbody>
+            <?php
+            while ($medicalConcernsRow = mysqli_fetch_assoc($medicalConcernsResults)) {
+                $medicalConcernTypeId = $medicalConcernsRow['Id'];
+                $medicalConcernName = $medicalConcernsRow['Medical_Concern_Name'];
+                $medicalConcernType = $medicalConcernsRow['Type_Name'];
+                $studentWithAllergy = $medicalConcernsRow['First_Name'] . " " . $medicalConcernsRow['Last_Name'];
+                ?>
+                <tr>
+                    <td class='align-middle'></td>
+                    <td class='text-center align-middle'><?php echo $medicalConcernName; ?></td>
+                    <td class='text-center align-middle'><?php echo $medicalConcernType; ?></td>
+                    <td class='text-center align-middle'><?php echo $studentWithAllergy; ?></td>
+                    <td class='text-center'>
+                        <div class='left-action-buttons-container d-inline m-auto'>
+                            <div class=' d-inline'>
+                                <button type='button'
+                                        class='btn large-action-buttons edit-button'
+                                        onclick='launchEditMedicalConcernsModal(<?php echo $medicalConcernTypeId; ?>)'
+                                >
+                                    <i class='fa fa-pencil'></i> Edit
+                                </button>
+                            </div>
+                            <div class='d-inline'>
+                                <button type='button'
+                                        class='btn large-action-buttons delete-button'
+                                        onclick='launchConfirmArchiveModal(
+                                                "<?php echo $medicalConcernTypeId; ?>",
+                                                "ArchiveMedicalConcern.php",
+                                                "Medical Concern Type",
+                                                "<?php echo $medicalConcernType; ?>",
+                                                "ShowMedicalConcerns.php")'
+                                >
+                                    <i class='fa fa-archive'></i> Archive
+                                </button>
+                            </div>
                         </div>
-                    </div>
-                </div>
-            </div>
 
-            <form class="container-fluid" method="POST" action="#" name="allProgramsTable" id="allProgramsTable">
-                <div class="table-responsive col-sm-12">
-                    <table id="search-table" class="table table-striped table-hover">
-                        <thead>
-                        <tr class="row">
-                            <th class="col-sm-1">#</th>
-                            <th class="col-sm-3">Medical Concerns</th>
-                            <th class="col-sm-2 text-center">Concern Type</th>
-                            <th class="col-sm-3 text-center">Students With Concern</th>
-                            <th class="col-sm-3 text-center">Actions</th>
-                        </tr>
-                        </thead>
-
-                        <tbody>
-                        <?php
-                        while ($medicalConcernsRow = mysqli_fetch_assoc($medicalConcernsResults)) {
-                            $medicalConcernTypeId = $medicalConcernsRow['Id'];
-                            $medicalConcernName = $medicalConcernsRow['Medical_Concern_Name'];
-                            $medicalConcernType = $medicalConcernsRow['Type_Name'];
-                            $studentWithAllergy = $medicalConcernsRow['First_Name'] . " " . $medicalConcernsRow['Last_Name'];
-                            ?>
-                            <tr class='row'>
-                                <td class='col-sm-1 align-middle'></td>
-                                <td class='col-sm-3 align-middle'><?php echo $medicalConcernName; ?></td>
-                                <td class='col-sm-2 text-center align-middle'><?php echo $medicalConcernType; ?></td>
-                                <td class='col-sm-3 text-center align-middle'><?php echo $studentWithAllergy; ?></td>
-                                <td class='col-sm-3 text-center'>
-                                    <div class='left-action-buttons-container d-inline m-auto'>
-                                        <div class=' d-inline'>
-                                            <button type='button'
-                                                    class='btn large-action-buttons edit-button'
-                                                    onclick='launchEditMedicalConcernsModal(<?php echo $medicalConcernTypeId; ?>)'
-                                            >
-                                                <i class='fa fa-pencil'></i> Edit
-                                            </button>
-                                        </div>
-                                        <div class='d-inline'>
-                                            <button type='button'
-                                                    class='btn large-action-buttons delete-button'
-                                                    onclick='launchConfirmArchiveModal(
-                                                        "<?php echo $medicalConcernTypeId; ?>",
-                                                        "ArchiveMedicalConcern.php",
-                                                        "Medical Concern Type",
-                                                        "<?php echo $medicalConcernType; ?>",
-                                                        "ShowMedicalConcerns.php")'
-                                            >
-                                                <i class='fa fa-archive'></i> Archive
-                                            </button>
-                                        </div>
-                                    </div>
-
-                                    <div class='right-action-buttons-container d-inline'>
-                                            <span title='Students With Concern' data-toggle='tooltip'
-                                                  class='small-action-buttons'>
-                                                <button type='button'
-                                                        onclick='launchStudentsWithMedicalConcernsModal(<?php echo $medicalConcernId; ?>)'
-                                                        class='btn small-action-buttons test-scores-button'>
-                                                        <i class='fa fa-graduation-cap mr-0'></i>
-                                                </button>
-                                            </span>
-                                    </div>
-                                </td>
-                            </tr>
-                        <?php } ?>
-                        </tbody>
-                    </table>
-                </div>
-            </form>
-
-            <div class="card-footer">
-
-            </div>
-
-        </div>
+                        <div class='right-action-buttons-container d-inline'>
+                            <span title='Students With Concern' data-toggle='tooltip' class='small-action-buttons'>
+                                <button type='button' class='btn small-action-buttons test-scores-button'
+                                        onclick='launchStudentsWithMedicalConcernsModal(<?php echo $medicalConcernId; ?>)'
+                                >
+                                    <i class='fa fa-graduation-cap mr-0'></i>
+                                </button>
+                            </span>
+                        </div>
+                    </td>
+                </tr>
+            <?php } ?>
+            </tbody>
+            <tfoot>
+            <tr>
+                <th>#</th>
+                <th class="text-center">Medical Concerns</th>
+                <th class="text-center">Concern Type</th>
+                <th class="text-center">Students With Concern</th>
+                <th class="text-center">Actions</th>
+            </tr>
+            </tfoot>
+        </table>
     </div>
+
+    <script type="text/javascript">
+        $(document).ready(function () {
+            $('#showMedicalConcernsTable').DataTable();
+        });
+    </script>
+</div>
+
 <?php
 include("../app-shell/Footer.php");
 ?>
