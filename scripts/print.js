ed365a58b0c90676392ee7171ab13390d7d53577
jcyarbro88 <jcyarbro88@gmail.com>
2018-05-07 00:22:31 -0500

    removed not used print files

--- scripts/print.js
+++
@@ -1,9 +0,0 @@
-function printReport(printable){
-    var former_page = document.body.innerHTML;
-    var printable_report = document.getElementById(printable).innerHTML;
-    
-    document.body.innerHTML = printable_report;
-    window.print();
-    document.body.innerHTML = former_page;
-}
-    
